6c4c6923eaedb8eae802fdf3d3c4baf582bbbb84
Heshy Roskes <heshyr@gmail.com>
2020-01-10 11:18:32 -0500

    successfully read the metadata

--- readtable.py
+++ readtable.py
@@ -1,12 +1,12 @@
 import collections, csv
 
-def readtable(filename, **columntypes):
-	result = []
-	with open(filename) as f:
-		reader = csv.DictReader(f)
-		Row = collections.namedtuple("Row", reader.fieldnames)
-		for row in reader:
-			for column, typ in columntypes.items():
-				row[column] = typ(row[column])
-			result.append(Row(**row))
-	return result
+def readtable(filename, rowname, **columntypes):
+  result = []
+  with open(filename) as f:
+    reader = csv.DictReader(f)
+    Row = collections.namedtuple(rowname, reader.fieldnames)
+    for row in reader:
+      for column, typ in columntypes.items():
+        row[column] = typ(row[column])
+      result.append(Row(**row))
+  return result
