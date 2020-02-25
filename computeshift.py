edb8d60dbaeb96190dfba4a7c565d19d340a1ae5
Heshy Roskes <heshyr@gmail.com>
2020-02-25 15:00:21 -0500

    attempt at error estimation... not quite right

--- computeshift.py
+++ computeshift.py
@@ -17,108 +17,64 @@ def computeshift(images, *, windowsize=10, smoothsigma=None, window=lambda image
   y, x = np.mgrid[0:invfourier.shape[0],0:invfourier.shape[1]]
   z = invfourier
 
+  #roll to get the peak in the middle
   x = np.roll(x, x.shape[0]//2, axis=0)
   y = np.roll(y, y.shape[0]//2, axis=0)
   z = np.roll(z, z.shape[0]//2, axis=0)
   x = np.roll(x, x.shape[1]//2, axis=1)
   y = np.roll(y, y.shape[1]//2, axis=1)
   z = np.roll(z, z.shape[1]//2, axis=1)
-  #roll to get the peak in the middle
 
-  #estimate gaussian mean and center
-  labels = (
-      (x < localmax_windowsize//2)
-    | (x > invfourier.shape[1]-localmax_windowsize//2)
-  ) & (
-      (y < localmax_windowsize//2)
-    | (y > invfourier.shape[0]-localmax_windowsize//2)
+  #change coordinate system, so 0 is in the middle
+  x[x > x.shape[1]/2] -= x.shape[1]
+  y[y > y.shape[0]/2] -= y.shape[0]
+
+  maxidx = np.unravel_index(np.argmax(np.abs(z)), z.shape)
+
+  slc = (
+    slice(maxidx[0]-windowsize, maxidx[0]+windowsize),
+    slice(maxidx[1]-windowsize, maxidx[1]+windowsize),
   )
-  maxindices = skimage.feature.peak_local_max(z, min_distance=localmax_min_distance, threshold_rel=localmax_threshold_rel, labels=labels)
-  results = []
-  for maxidx in maxindices:
-    maxidx = tuple(maxidx)
-    try:
-      mux = x[maxidx]
-      muy = y[maxidx]
-      A = z[maxidx]
+  xx = x[slc]
+  yy = y[slc]
+  zz = z[slc]
 
-      #estimate gaussian width
-      bigpoints = np.argwhere(z>=np.max(z)/2)
-      distances = np.linalg.norm(bigpoints-maxidx, axis=1)
-      covxx = covyy = max(distances[distances <= windowsize * 2**.5])**2
-      covxy = 0.
+  if showbigimage: plt.imshow(z)
+  if showsmallimage: plt.imshow(zz)
 
-      p0 = unp.nominal_values(np.array([mux, muy, covxx, covyy, covxy, A]))
+  xx = np.ravel(xx)
+  yy = np.ravel(yy)
+  zz = np.ravel(zz)
 
-      slc = (
-        slice(maxidx[0]-windowsize, maxidx[0]+windowsize),
-        slice(maxidx[1]-windowsize, maxidx[1]+windowsize),
-      )
-      xx = x[slc]
-      yy = y[slc]
-      zz = z[slc]
+  knotsx = ()
+  knotsy = ()
+  spline = scipy.interpolate.LSQBivariateSpline(xx, yy, zz, knotsx, knotsy)
+  def f(*args, **kwargs): return spline(*args, **kwargs)[0,0]
 
-      if showbigimage: plt.imshow(z)
-      if showsmallimage: plt.imshow(zz)
+  r = scipy.optimize.minimize(
+    fun=lambda xy: -f(*xy),
+    x0=(x[maxidx], y[maxidx]),
+    jac=lambda xy: np.array([-f(*xy, dx=1), -f(*xy, dy=1)]),
+    bounds=((x[maxidx]-windowsize, x[maxidx]+windowsize), (y[maxidx]-windowsize, y[maxidx]+windowsize)),
+    method="TNC",
+  )
 
-      xx = np.ravel(xx)
-      yy = np.ravel(yy)
-      zz = np.ravel(zz)
+  hessian = -np.array([
+    [f(*r.x, dx=2, dy=0), f(*r.x, dx=1, dy=1)],
+    [f(*r.x, dx=1, dy=1), f(*r.x, dx=0, dy=2)],
+  ])
+  covariance = f(*r.x) * np.linalg.inv(hessian)
 
-      f = functools.partial(vectorizedperiodicdoublegaussian, shape=invfourier.shape)
+  dx, dy = unc.correlated_values(
+    -r.x,
+    covariance
+  )
 
-      xfit = np.array([xx, yy], order="F")
-      p, cov = scipy.optimize.curve_fit(
-        f,
-        xfit,
-        zz,
-        p0=p0,
-      )
-      mux, muy, covxx, covyy, covxy, A = unc.correlated_values(p, cov)
-
-      logger.info("%s %s %s %s %s %s %d", mux, muy, covxx, covyy, covxy, A, windowsize)
-
-      dx, dy = unc.correlated_values(
-        [-mux.n, -muy.n],
-        unc.covariance_matrix([mux, muy]) + np.array([[covxx.n, covxy.n], [covxy.n, covyy.n]]),
-      )
-
-      while dx.n >= invfourier.shape[1] / 2: dx -= invfourier.shape[1]
-      while dx.n < -invfourier.shape[1] / 2: dx += invfourier.shape[1]
-      while dy.n >= invfourier.shape[0] / 2: dy -= invfourier.shape[0]
-      while dy.n < -invfourier.shape[0] / 2: dy += invfourier.shape[0]
-
-      chi2 = sum((f(xfit, *p) - zz) ** 2)
-
-      results.append(OptimizeResult(
-        mux=mux,
-        muy=muy,
-        covxx=covxx,
-        covyy=covyy,
-        covxy=covxy,
-        A=A,
-        p0=p0,
-        dx=dx,
-        dy=dy,
-        chi2=chi2,
-      ))
-    except Exception as e:
-      dx, dy = unc.correlated_values([0, 0], [[9999, 0], [0, 9999]])
-      results.append(OptimizeResult(
-        exception=e,
-      ))
-
-  def resultevaluator(result):
-    if hasattr(result, "exception"): return "bad", 0
-    cov = unc.covariance_matrix([result.dx, result.dy])
-    if np.trace(cov) ** .5 >= sum(invfourier.shape): return "bad", 1
-    return "ok", -result.chi2
-
-  result = max(results, key=resultevaluator)
-  if hasattr(result, "exception"): raise result.exception
-  results.remove(result)
-  result.otherresults = results
-  return result
+  return OptimizeResult(
+    dx=dx,
+    dy=dy,
+    spline=spline,
+  )
 
 @nb.njit
 def hann(images):
@@ -131,30 +87,6 @@ def hann(images):
 @nb.njit
 def getcrosspower(fourier):
   return fourier[0] * np.conj(fourier[1])
-
-@nb.njit
-def doublegaussian(x, mux, muy, invcov, A):
-    mu = np.array([mux, muy])
-    shifted = x - mu
-
-    return A * np.exp(-0.5 * shifted @ invcov @ shifted)
-
-@nb.njit
-def periodicdoublegaussian(x, mux, muy, invcov, A, shape):
-    result = 0
-    shape0, shape1 = shape
-    for i in -1, 0, 1:
-        for j in -1, 0, 1:
-            result += doublegaussian(x, mux+i*shape1, muy+j*shape0, invcov, A)
-    return result
-
-def vectorizedperiodicdoublegaussian(x, mux, muy, covxx, covyy, covxy, A, shape):
-    cov = np.array([[covxx, covxy], [covxy, covyy]])
-    invcov = np.linalg.inv(cov)
-
-    result = np.apply_along_axis(periodicdoublegaussian, 0, x, mux, muy, invcov, A, shape)
-
-    return result
 
 @nb.njit
 def mse(a):
