9a87bc4c003a8858bba8d136ec347ff4e669bdda
Heshy Roskes <heshyr@gmail.com>
2020-03-05 09:35:58 -0500

    compare to cvxpy stitching

--- test/testalignment.py
+++ test/testalignment.py
@@ -1,4 +1,4 @@
-import dataclasses, numbers, numpy as np, os, unittest
+import dataclasses, numbers, numpy as np, os, uncertainties.unumpy as unp, unittest
 from ..alignmentset import AlignmentSet, ImageStats, StitchCoordinate, StitchCovarianceEntry
 from ..overlap import AlignmentResult
 from ..tableio import readtable
@@ -18,17 +18,18 @@ def assertAlmostEqual(a, b, **kwargs):
     return np.testing.assert_equal(a, b)
 
 class TestAlignment(unittest.TestCase):
+  @classmethod
+  def setUpClass(cls):
+    a = cls.a = AlignmentSet(os.path.join(thisfolder, "data"), os.path.join(thisfolder, "data", "flatw"), "M21_1")
+    a.getDAPI()
+    a.align(debug=True, errorfactor=1/5)
+    cls.stitchresult = a.stitch()
+
   def setUp(self):
     pass
 
   def tearDown(self):
     pass
-
-  def testAlign(self):
-    a = AlignmentSet(os.path.join(thisfolder, "data"), os.path.join(thisfolder, "data", "flatw"), "M21_1")
-    a.getDAPI()
-    a.align(debug=True, errorfactor=1/5)
-    a.stitch()
 
   def testAlignmentResults(self):
     for filename, cls in (
@@ -41,3 +42,15 @@ class TestAlignment(unittest.TestCase):
       targetrows = readtable(os.path.join(thisfolder, "alignmentreference", filename), cls)
       for row, target in zip(rows, targetrows):
         assertAlmostEqual(row, target, rtol=1e-5)
+
+  def testStitchCvxpy(self):
+    cvxpyresult = self.a.stitch(saveresult=False, usecvxpy=True)
+
+    np.testing.assert_allclose(cvxpyresult.x(), unp.nominal_values(self.stitchresult.x()))
+    np.testing.assert_allclose(cvxpyresult.T,   unp.nominal_values(self.stitchresult.T  ))
+
+    x = np.concatenate((self.stitchresult.x(), self.stitchresult.T), axis=None)
+    np.testing.assert_close(
+      cvxpyresult.problem.value,
+      x @ self.stitchresult.A @ x + self.stitchresult.b @ x + self.stitchresult.c
+    )
