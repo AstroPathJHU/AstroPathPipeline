f4320a920482e9f8773a14860e8b55bac04ab98a
Heshy Roskes <heshyr@gmail.com>
2020-03-06 14:33:34 -0500

    start to test symmetry when aligning opposite overlaps

--- test/testalignment.py
+++ test/testalignment.py
@@ -1,5 +1,6 @@
 import dataclasses, itertools, numbers, numpy as np, os, uncertainties.unumpy as unp, unittest
 from ..alignmentset import AlignmentSet, ImageStats, StitchCoordinate, StitchCovarianceEntry
+from ..computeshift import crosscorrelation
 from ..overlap import AlignmentResult
 from ..tableio import readtable
 
@@ -19,14 +20,15 @@ def assertAlmostEqual(a, b, **kwargs):
 
 class TestAlignment(unittest.TestCase):
   def setUp(self):
-    self.a = AlignmentSet(os.path.join(thisfolder, "data"), os.path.join(thisfolder, "data", "flatw"), "M21_1")
+    pass
 
   def tearDown(self):
     pass
 
   def testAlignment(self):
-    self.a.getDAPI()
-    self.a.align(debug=True, errorfactor=1/5)
+    a = AlignmentSet(os.path.join(thisfolder, "data"), os.path.join(thisfolder, "data", "flatw"), "M21_1")
+    a.getDAPI()
+    a.align(debug=True, errorfactor=1/5)
     for filename, cls in (
       ("M21_1_imstat.csv", ImageStats),
       ("M21_1_align.csv", AlignmentResult),
@@ -39,24 +41,26 @@ class TestAlignment(unittest.TestCase):
         assertAlmostEqual(row, target, rtol=1e-5)
 
   def testReadAlignment(self):
+    a = AlignmentSet(os.path.join(thisfolder, "data"), os.path.join(thisfolder, "data", "flatw"), "M21_1")
     readfilename = os.path.join(thisfolder, "alignmentreference", "M21_1_align.csv")
     writefilename = os.path.join(thisfolder, "testreadalignments.csv")
 
-    self.a.readalignments(filename=readfilename)
-    self.a.writealignments(filename=writefilename)
+    a.readalignments(filename=readfilename)
+    a.writealignments(filename=writefilename)
     rows = readtable(writefilename, AlignmentResult)
     targetrows = readtable(readfilename, AlignmentResult)
     for row, target in itertools.zip_longest(rows, targetrows):
       assertAlmostEqual(row, target, rtol=1e-5)
 
   def testStitchCvxpy(self):
-    self.a.readalignments(filename=os.path.join(thisfolder, "alignmentreference", "M21_1_align.csv"))
+    a = AlignmentSet(os.path.join(thisfolder, "data"), os.path.join(thisfolder, "data", "flatw"), "M21_1")
+    a.readalignments(filename=os.path.join(thisfolder, "alignmentreference", "M21_1_align.csv"))
 
-    defaultresult = self.a.stitch()
-    cvxpyresult = self.a.stitch(saveresult=False, usecvxpy=True)
+    defaultresult = a.stitch()
+    cvxpyresult = a.stitch(saveresult=False, usecvxpy=True)
 
-    centerresult = self.a.stitch(saveresult=False, fixpoint="center")
-    centercvxpyresult = self.a.stitch(saveresult=False, fixpoint="center", usecvxpy=True)
+    centerresult = a.stitch(saveresult=False, fixpoint="center")
+    centercvxpyresult = a.stitch(saveresult=False, fixpoint="center", usecvxpy=True)
 
     np.testing.assert_allclose(centercvxpyresult.x(), unp.nominal_values(centerresult.x()), rtol=1e-3)
     np.testing.assert_allclose(centercvxpyresult.T,   unp.nominal_values(centerresult.T  ), rtol=1e-3, atol=1e-3)
@@ -85,3 +89,10 @@ class TestAlignment(unittest.TestCase):
       unp.nominal_values(centerresult.x() - centerresult.x()[0]),
       rtol=1e-4,
     )
+
+  def testSymmetry(self):
+    a = AlignmentSet(os.path.join(thisfolder, "data"), os.path.join(thisfolder, "data", "flatw"), "M21_1", selectrectangles=(10, 11))
+    a.getDAPI()
+    o1, o2 = a.overlaps
+    c1, c2 = crosscorrelation(o1.cutimages), crosscorrelation(o2.cutimages)
+    np.testing.assert_allclose(np.roll(np.roll(np.fft.ifft2(c2).real[::-1,::-1], 1, axis=0), 1, axis=1), np.fft.ifft2(c1).real)
