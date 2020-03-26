c3d9af8eb46731348a0133acdc75cca0f76487d3
Heshy Roskes <heshyr@gmail.com>
2020-03-26 15:51:57 -0400

    don't ruin the pdf

--- utilities.py
+++ utilities.py
@@ -4,13 +4,5 @@ def covariance_matrix(*args, **kwargs):
   result = np.array(unc.covariance_matrix(*args, **kwargs))
   return (result + result.T) / 2
 
-def removepdfpagegroup(filename):
-  with open(filename, "rb") as f:
-    content = f.read()
-  content = content.replace(b"/Group << /CS /DeviceRGB /S /Transparency /Type /Group >>\n", b"")
-  with open(filename, "wb") as f:
-    f.write(content)
-
 def savefig(fname, *args, **kwargs):
   plt.savefig(fname, *args, **kwargs)
-  removepdfpagegroup(fname)
