fd6f371be4bdfd2131bbb7f9b8e4128ed554f91e
Maggie Eminizer <margaret.eminizer@gmail.com>
2020-05-26 15:40:32 -0400

    can input raw filename log from a previous run to repeat
    also the samplenames file is now a .txt with line break separators instead of a .csv

--- flatfield/run_flatfield.py
+++ flatfield/run_flatfield.py
@@ -5,7 +5,7 @@ from ..utilities.img_file_io import getImageHWLFromXMLFile
 from ..utilities.misc import cd, split_csv_to_list
 from .utilities import sampleNameFromFilepath
 from argparse import ArgumentParser
-import os, glob, csv, random
+import os, glob, random
 
 #################### HELPER FUNCTIONS ####################
 
@@ -24,34 +24,38 @@ def checkArgs(a) :
     if a.skip_masking and a.save_masking_plots :
         raise ValueError('ERROR: cannot save masking plots if masking will be skipped')
 
-#helper function to get the list of sample names to run on
-def getAllSampleNames(a) :
-    #get them from the .csv file if requested
-    if '.csv' in a.samplenames :
-        #make sure that the samplename CSV file exists
-        if not os.path.isfile(a.samplenames) :
-            raise ValueError(f'ERROR: sample name CSV file {a.samplenames} does not exist!')
-        with open(a.samplenames,) as f:
-            reader = csv.reader(f)
-            all_sample_names = (list(reader))[0]
-    #otherwise they should just be listed in the command line argument
-    else :
-        all_sample_names = split_csv_to_list(a.samplenames)
-    return all_sample_names
-
 #helper function to get the list of filepaths and associated sample names to run on based on the selection method and number of images requested
 def getFilepathsAndSampleNamesToRun(a) :
-    #get all the possible sample names
-    all_sample_names = getAllSampleNames(a)
-    #make sure the rawfile and dbload directories all exist
+    samplenames_to_run = None; filepaths_to_run = None
+    #get sample names/filepaths from the .csv file if requested
+    if '.txt' in a.samplenames :
+        #make sure that the CSV file exists
+        if not os.path.isfile(a.samplenames) :
+            raise ValueError(f'ERROR: sample name CSV file {a.samplenames} does not exist!')
+        #get the file contents
+        with open(a.samplenames,'r') as f:
+            file_lines = [l.rstrip() for l in f.readlines()]
+        #if the input file was filepaths, then each entry should have the rawfile directory in it
+        if a.rawfile_top_dir.split(os.sep)[-1] in file_lines[0].split(os.sep) :
+            filepaths_to_run = file_lines
+            flatfield_logger.info(f'Will run on a sample of {len(filepaths_to_run)} total files as listed in {a.samplenames}')
+            samplenames_to_run = list(set([sampleNameFromFilepath(fp) for fp in filepaths_to_run]))
+            all_sample_names = samplenames_to_run
+        #otherwise the input file is sample names
+        else :
+            all_sample_names = file_lines
+    #if the sample/filenames weren't given in a .csv the samples should just be listed in the command line argument
+    else :
+        all_sample_names = split_csv_to_list(a.samplenames)
+    #get the filepaths of all the images in the samples to run, regardless of how the files to run on will themselves be chosen
+    all_image_filepaths=[]
+    #make sure the rawfile and dbload directories all exist first
     for sn in all_sample_names :
         if not os.path.isdir(os.path.join(a.rawfile_top_dir,sn)) :
             raise ValueError(f'ERROR: sample directory {os.path.join(a.rawfile_top_dir,sn)} does not exist!')
         if not os.path.isdir(os.path.join(a.dbload_top_dir,sn,'dbload')) :
-            raise ValueError(f'ERROR: dbload directory for sample {sn} does not exist!')
-    #get the (sorted) full list of file names in the sample to choose from
-    all_image_filepaths = []
-    #iterate over the samples
+            raise ValueError(f"ERROR: dbload directory {os.path.join(a.dbload_top_dir,sn,'dbload')} for sample {sn} does not exist!")
+    #get the (sorted) full list of file names in each sample to choose from
     for sn in all_sample_names :
         #get the list of all the filenames for this sample
         with cd(os.path.join(a.rawfile_top_dir,sn)) :
@@ -59,28 +63,29 @@ def getFilepathsAndSampleNamesToRun(a) :
                                             for fn in glob.glob(f'{sn}_[[]*,*[]]{a.rawfile_ext}')]
             all_image_filepaths+=this_sample_image_filepaths
     all_image_filepaths.sort()
-    #select the total filepaths/sample names to run on
-    filepaths_to_run = None
-    logstring='Will run on a sample of'
-    if min(a.max_images,len(all_image_filepaths)) in [-1,len(all_image_filepaths)] :
-        logstring+=f' all {len(all_image_filepaths)} images' 
-        if min(a.max_images,len(all_image_filepaths)) == len(all_image_filepaths) :
-            logstring+=f' (not enough images in the sample(s) to deliver all {a.max_images} requested)'
-        filepaths_to_run = all_image_filepaths
-    else :
-        if a.selection=='first' :
-            filepaths_to_run=all_image_filepaths[:a.max_images]
-            logstring+=f' the first {a.max_images} images'
-        elif a.selection=='last' :
-            filepaths_to_run=all_image_filepaths[-(a.max_images):]
-            logstring+=f' the last {a.max_images} images'
-        elif a.selection=='random' :
-            random.shuffle(all_image_filepaths)
-            filepaths_to_run=all_image_filepaths[:a.max_images]
-            logstring+=f' {a.max_images} randomly-chosen images'
-    flatfield_logger.info(logstring)
-    #figure out the samples from which those files are coming
-    samplenames_to_run = list(set([sampleNameFromFilepath(fp) for fp in filepaths_to_run]))
+    #if the rawfiles haven't already been selected, figure that out
+    if filepaths_to_run is None :
+        #select the total filepaths/sample names to run on
+        logstring='Will run on a sample of'
+        if min(a.max_images,len(all_image_filepaths)) in [-1,len(all_image_filepaths)] :
+            logstring+=f' all {len(all_image_filepaths)} images' 
+            if min(a.max_images,len(all_image_filepaths)) == len(all_image_filepaths) :
+                logstring+=f' (not enough images in the sample(s) to deliver all {a.max_images} requested)'
+            filepaths_to_run = all_image_filepaths
+        else :
+            if a.selection=='first' :
+                filepaths_to_run=all_image_filepaths[:a.max_images]
+                logstring+=f' the first {a.max_images} images'
+            elif a.selection=='last' :
+                filepaths_to_run=all_image_filepaths[-(a.max_images):]
+                logstring+=f' the last {a.max_images} images'
+            elif a.selection=='random' :
+                random.shuffle(all_image_filepaths)
+                filepaths_to_run=all_image_filepaths[:a.max_images]
+                logstring+=f' {a.max_images} randomly-chosen images'
+        flatfield_logger.info(logstring)
+        #figure out the samples from which those files are coming
+        samplenames_to_run = list(set([sampleNameFromFilepath(fp) for fp in filepaths_to_run]))
     logstring = f'Background threshold will be determined from images in {len(samplenames_to_run)}'
     if len(samplenames_to_run)>1 :
         logstring+=' different samples:'
@@ -99,7 +104,8 @@ def main() :
     #define and get the command-line arguments
     parser = ArgumentParser()
     #positional arguments
-    parser.add_argument('samplenames',     help='Comma-separated list of sample names to include (or path to the csv file that lists them)')
+    parser.add_argument('samplenames',     help="""List of sample names to include (or path to the file that lists either 
+                                                   sample names or the files from a previous run, one per line)""")
     parser.add_argument('rawfile_top_dir', help='Path to directory that holds each of the [samplename] directories that contain raw files')
     parser.add_argument('dbload_top_dir',  help="""Path to directory that holds each of the [samplename]/dbload directories 
                                                    which contain *_overlap.csv and *_rect.csv files""")
