751dcbf43248b361df5dec1753b7f366570f8dec
Maggie Eminizer <margaret.eminizer@gmail.com>
2020-06-15 21:11:49 -0400

    no wait, THIS one!

--- test/testflatfield.py
+++ test/testflatfield.py
@@ -29,7 +29,7 @@ dims=getImageHWLFromXMLFile(rawfile_top_dir,samp)
 print('Initializing FlatfieldProducer')
 ff_producer = FlatfieldProducer(dims,sample_names_to_run,filepaths_to_run,dbload_top_dir,working_dir,False)
 #write out the text file of all the raw file paths that will be run
-ff_producer.writeFileLog()
+ff_producer.writeFileLog('filepath_log.txt')
 #mask and stack images together
 ff_producer.stackImages(1,0.0,0,True)
 #make the flatfield image
