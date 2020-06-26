8bc011e63b55fd82eb6c06936811d44978c4f9a0
Heshy Roskes <heshyr@gmail.com>
2020-06-26 15:51:42 -0400

    move function down

--- alignment/plots.py
+++ alignment/plots.py
@@ -72,6 +72,131 @@ def plotpairwisealignments(alignmentset, *, stitched=False, tags=[1, 2, 3, 4, 6,
   logger.debug("done")
   return vectors
 
+def closedlooppulls(alignmentset, *, tagsequence, binning=np.linspace(-5, 5, 51), quantileforstats=1, verbose=True, stitchresult=None, saveas=None, figurekwargs={}, plotstyling=lambda fig, ax: None):
+  logger.debug(alignmentset.samp)
+  dct = {
+    1: (-1, -1),
+    2: ( 0, -1),
+    3: ( 1, -1),
+    4: (-1,  0),
+    6: ( 1,  0),
+    7: (-1,  1),
+    8: ( 0,  1),
+    9: ( 1,  1),
+  }
+  totaloffset = sum(np.array(dct[tag]) for tag in tagsequence)
+  if np.any(totaloffset):
+    raise ValueError(f"please check your tag sequence - it ends with an offset of {tuple(totaloffset)}")
+
+  overlaps = alignmentset.overlaps
+  g = alignmentset.overlapgraph()
+  xresiduals, yresiduals = [], []
+  overlapdict = nx.get_edge_attributes(g, "overlap")
+
+  for o in overlaps:
+    if o.result.exit: continue
+    if o.tag != tagsequence[-1]: continue
+    for path in nx.algorithms.simple_paths.shortest_simple_paths(g, o.p2, o.p1):
+      if len(path) < len(tagsequence): continue
+      if len(path) > len(tagsequence): break
+      path = path[:]
+      path.append(path[0]) #make a full circle
+
+      overlaps = [overlapdict[nodepair] for nodepair in pairwise(path)]
+      assert overlaps[-1] is o
+      tags = [o.tag for o in overlaps]
+      if tags != tagsequence: continue
+
+      if any(o.result.exit for o in overlaps): continue
+      if stitchresult is not None:
+        dxvecs = [stitchresult.dx(o) for o in overlaps]
+      else:
+        dxvecs = [o.result.dxvec for o in overlaps]
+
+      dxs, dys = zip(*dxvecs)
+
+      if verbose is True or verbose is not False and verbose(path, dxs, dys):
+        print(" --> ".join(f"{node:4d}" for node in path))
+        for nodepair, dx, dy in zip(pairwise(path), dxs, dys):
+          print(f"  {nodepair[0]:4d} --> {nodepair[1]:4d}: {dx:10} {dy:10}")
+        print(f"          total: {sum(dxs):10} {sum(dys):10}")
+
+      totaldx = sum(dxs)
+      totaldy = sum(dys)
+      xresiduals.append(totaldx)
+      yresiduals.append(totaldy)
+
+  if verbose:
+    print()
+    print()
+    print()
+  fig = plt.figure(**figurekwargs)
+  ax = fig.add_subplot(1, 1, 1)
+  print("x pulls:")
+  pullhist(xresiduals, binning=binning, verbose=True, alpha=0.5, label="$x$ pulls", stdinlabel=True)
+  print()
+  print()
+  print()
+  print("y pulls:")
+  pullhist(yresiduals, binning=binning, verbose=True, alpha=0.5, label="$y$ pulls", stdinlabel=True)
+  plotstyling(fig=fig, ax=ax)
+
+  if saveas is not None:
+    plt.savefig(saveas)
+    plt.close()
+
+  logger.debug("done")
+  return xresiduals, yresiduals
+
+def shiftplot2D(alignmentset, *, saveasx=None, saveasy=None, figurekwargs={}, plotstyling=lambda fig, ax, cbar, xory: None, island=None, showplot=None):
+  logger.debug(alignmentset.samp)
+  fields = alignmentset.fields
+  if island is not None:
+    fields = [field for field in fields if field.n in alignmentset.islands()[island]]
+  deltax = min(abs(a.x-b.x) for a, b in more_itertools.pairwise(fields) if a.x != b.x)
+  deltay = min(abs(a.y-b.y) for a, b in more_itertools.pairwise(fields) if a.y != b.y)
+  deltaxvec = deltax, deltay
+  x0vec = np.min([[f.x, f.y] for f in fields], axis=0)
+  f = fields[0]
+  shape = tuple(reversed(floattoint(np.max([(f.xvec - x0vec) / deltaxvec for f in fields], axis=0), atol=1e-9) + 1))
+  xyarray = np.full(shape=(2,)+shape, fill_value=units.Distance(pixels=-999., pscale=alignmentset.pscale))
+
+  extent = x0vec[0], shape[1] * deltaxvec[0] + x0vec[0], shape[0] * deltaxvec[1] + x0vec[1], x0vec[1]
+
+  for f in fields:
+    idx = (slice(None),) + tuple(reversed(floattoint((f.xvec - x0vec) / deltaxvec, atol=1e-9)))
+    xyarray[idx] = units.nominal_values(f.pxvec - alignmentset.T@f.xvec)
+
+  xyarraypixels = units.pixels(xyarray, pscale=alignmentset.pscale)
+
+  vmin = min(np.min(xyarraypixels[xyarraypixels != -999]), -np.max(xyarraypixels[xyarraypixels != -999]))
+  vmax = -vmin
+  norm = colors.Normalize(vmin=vmin, vmax=vmax)
+  cmap = cm.get_cmap()
+  xycolor = cmap(norm(xyarraypixels))
+  xycolor[xyarraypixels == -999] = 0
+
+  if showplot is None: showplot = saveasx is saveasy is None
+
+  for colorplot, xory, saveas in zip(xycolor, "xy", (saveasx, saveasy)):
+    fig = plt.figure(**figurekwargs)
+    ax = plt.gca()
+    ax.imshow(colorplot, extent=units.pixels(extent, pscale=alignmentset.pscale))
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="5%", pad=0.05)
+    cbar = plt.colorbar(cm.ScalarMappable(norm=norm, cmap=cmap), cax=cax)
+    plotstyling(fig=fig, ax=ax, cbar=cbar, xory=xory)
+    if showplot:
+      plt.show()
+    if saveas is not None:
+      plt.savefig(saveas)
+    if not showplot:
+      plt.close()
+
+  logger.debug("done")
+  return xyarray, extent
+
+
 def shiftplotprofile(alignmentset, *, deltaxory, vsxory, saveas=None, figurekwargs={}, plotstyling=lambda fig, ax, deltaxory, vsxory: None, drawfourier=False, guessparameters=None, plotsine=True, sinetext=True, **kwargs):
   fig = plt.figure(**figurekwargs)
   ax = fig.add_subplot(1, 1, 1)
@@ -277,127 +402,3 @@ def shiftplotprofile(alignmentset, *, deltaxory, vsxory, saveas=None, figurekwar
 
   logger.debug("done")
   return x, y, yerr, p
-
-def closedlooppulls(alignmentset, *, tagsequence, binning=np.linspace(-5, 5, 51), quantileforstats=1, verbose=True, stitchresult=None, saveas=None, figurekwargs={}, plotstyling=lambda fig, ax: None):
-  logger.debug(alignmentset.samp)
-  dct = {
-    1: (-1, -1),
-    2: ( 0, -1),
-    3: ( 1, -1),
-    4: (-1,  0),
-    6: ( 1,  0),
-    7: (-1,  1),
-    8: ( 0,  1),
-    9: ( 1,  1),
-  }
-  totaloffset = sum(np.array(dct[tag]) for tag in tagsequence)
-  if np.any(totaloffset):
-    raise ValueError(f"please check your tag sequence - it ends with an offset of {tuple(totaloffset)}")
-
-  overlaps = alignmentset.overlaps
-  g = alignmentset.overlapgraph()
-  xresiduals, yresiduals = [], []
-  overlapdict = nx.get_edge_attributes(g, "overlap")
-
-  for o in overlaps:
-    if o.result.exit: continue
-    if o.tag != tagsequence[-1]: continue
-    for path in nx.algorithms.simple_paths.shortest_simple_paths(g, o.p2, o.p1):
-      if len(path) < len(tagsequence): continue
-      if len(path) > len(tagsequence): break
-      path = path[:]
-      path.append(path[0]) #make a full circle
-
-      overlaps = [overlapdict[nodepair] for nodepair in pairwise(path)]
-      assert overlaps[-1] is o
-      tags = [o.tag for o in overlaps]
-      if tags != tagsequence: continue
-
-      if any(o.result.exit for o in overlaps): continue
-      if stitchresult is not None:
-        dxvecs = [stitchresult.dx(o) for o in overlaps]
-      else:
-        dxvecs = [o.result.dxvec for o in overlaps]
-
-      dxs, dys = zip(*dxvecs)
-
-      if verbose is True or verbose is not False and verbose(path, dxs, dys):
-        print(" --> ".join(f"{node:4d}" for node in path))
-        for nodepair, dx, dy in zip(pairwise(path), dxs, dys):
-          print(f"  {nodepair[0]:4d} --> {nodepair[1]:4d}: {dx:10} {dy:10}")
-        print(f"          total: {sum(dxs):10} {sum(dys):10}")
-
-      totaldx = sum(dxs)
-      totaldy = sum(dys)
-      xresiduals.append(totaldx)
-      yresiduals.append(totaldy)
-
-  if verbose:
-    print()
-    print()
-    print()
-  fig = plt.figure(**figurekwargs)
-  ax = fig.add_subplot(1, 1, 1)
-  print("x pulls:")
-  pullhist(xresiduals, binning=binning, verbose=True, alpha=0.5, label="$x$ pulls", stdinlabel=True)
-  print()
-  print()
-  print()
-  print("y pulls:")
-  pullhist(yresiduals, binning=binning, verbose=True, alpha=0.5, label="$y$ pulls", stdinlabel=True)
-  plotstyling(fig=fig, ax=ax)
-
-  if saveas is not None:
-    plt.savefig(saveas)
-    plt.close()
-
-  logger.debug("done")
-  return xresiduals, yresiduals
-
-def shiftplot2D(alignmentset, *, saveasx=None, saveasy=None, figurekwargs={}, plotstyling=lambda fig, ax, cbar, xory: None, island=None, showplot=None):
-  logger.debug(alignmentset.samp)
-  fields = alignmentset.fields
-  if island is not None:
-    fields = [field for field in fields if field.n in alignmentset.islands()[island]]
-  deltax = min(abs(a.x-b.x) for a, b in more_itertools.pairwise(fields) if a.x != b.x)
-  deltay = min(abs(a.y-b.y) for a, b in more_itertools.pairwise(fields) if a.y != b.y)
-  deltaxvec = deltax, deltay
-  x0vec = np.min([[f.x, f.y] for f in fields], axis=0)
-  f = fields[0]
-  shape = tuple(reversed(floattoint(np.max([(f.xvec - x0vec) / deltaxvec for f in fields], axis=0), atol=1e-9) + 1))
-  xyarray = np.full(shape=(2,)+shape, fill_value=units.Distance(pixels=-999., pscale=alignmentset.pscale))
-
-  extent = x0vec[0], shape[1] * deltaxvec[0] + x0vec[0], shape[0] * deltaxvec[1] + x0vec[1], x0vec[1]
-
-  for f in fields:
-    idx = (slice(None),) + tuple(reversed(floattoint((f.xvec - x0vec) / deltaxvec, atol=1e-9)))
-    xyarray[idx] = units.nominal_values(f.pxvec - alignmentset.T@f.xvec)
-
-  xyarraypixels = units.pixels(xyarray, pscale=alignmentset.pscale)
-
-  vmin = min(np.min(xyarraypixels[xyarraypixels != -999]), -np.max(xyarraypixels[xyarraypixels != -999]))
-  vmax = -vmin
-  norm = colors.Normalize(vmin=vmin, vmax=vmax)
-  cmap = cm.get_cmap()
-  xycolor = cmap(norm(xyarraypixels))
-  xycolor[xyarraypixels == -999] = 0
-
-  if showplot is None: showplot = saveasx is saveasy is None
-
-  for colorplot, xory, saveas in zip(xycolor, "xy", (saveasx, saveasy)):
-    fig = plt.figure(**figurekwargs)
-    ax = plt.gca()
-    ax.imshow(colorplot, extent=units.pixels(extent, pscale=alignmentset.pscale))
-    divider = make_axes_locatable(ax)
-    cax = divider.append_axes("right", size="5%", pad=0.05)
-    cbar = plt.colorbar(cm.ScalarMappable(norm=norm, cmap=cmap), cax=cax)
-    plotstyling(fig=fig, ax=ax, cbar=cbar, xory=xory)
-    if showplot:
-      plt.show()
-    if saveas is not None:
-      plt.savefig(saveas)
-    if not showplot:
-      plt.close()
-
-  logger.debug("done")
-  return xyarray, extent
