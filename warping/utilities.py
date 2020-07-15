d91f2ba11af5c8cbde62d0d7cb76d1fdc5e8159b
Maggie Eminizer <margaret.eminizer@gmail.com>
2020-07-15 18:18:51 -0400

    fix overlap import

--- warping/utilities.py
+++ warping/utilities.py
@@ -2,7 +2,7 @@
 from .config import CONST
 from ..alignment.alignmentset import AlignmentSet
 from ..utilities.img_file_io import getImageHWLFromXMLFile, getRawAsHWL, writeImageToFile
-from ..prepdb.overlap import rectangleoverlaplist_fromcsvs
+from ..baseclasses.overlap import rectangleoverlaplist_fromcsvs
 from ..utilities.misc import cd
 import numpy as np, multiprocessing as mp
 import cv2, os, logging, glob, shutil, dataclasses
