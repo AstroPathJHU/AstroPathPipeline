17f0515ac6bf0661ba700290d0272feaef923f3d
Heshy Roskes <heshyr@gmail.com>
2020-07-16 12:02:19 -0400

    debugging plots

--- badregions/dustspeck.py
+++ badregions/dustspeck.py
@@ -1,22 +1,42 @@
-import cv2, numpy as np, scipy.ndimage
+import cv2, matplotlib.pyplot as plt, numpy as np, scipy.ndimage
 from ..flatfield.utilities import getImageArrayLayerHistograms, getLayerOtsuThresholdsAndWeights
 from .badregions import BadRegionFinder
 
 class DustSpeckFinder(BadRegionFinder):
-  def badregions(self, *, dilatesize=None, statserodesize=None):
+  def badregions(self, *, dilatesize=None, statserodesize=None, showdebugplots=False):
     hist = getImageArrayLayerHistograms(self.image)
     thresholds, weights = getLayerOtsuThresholdsAndWeights(hist)
     threshold = thresholds[1]  #first one finds signal, second finds dust speck
 
     badregions = cv2.UMat((self.image > threshold).astype(np.uint8))
+    if showdebugplots:
+      print("image > threshold")
+      plt.imshow(badregions.get())
+      plt.show()
 
     ellipse = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
     badregions = cv2.morphologyEx(badregions, cv2.MORPH_CLOSE, ellipse, borderType=cv2.BORDER_REPLICATE)
+    if showdebugplots:
+      print("after small close")
+      plt.imshow(badregions.get())
+      plt.show()
     badregions = cv2.morphologyEx(badregions, cv2.MORPH_OPEN,  ellipse, borderType=cv2.BORDER_REPLICATE)
+    if showdebugplots:
+      print("after small open")
+      plt.imshow(badregions.get())
+      plt.show()
 
     ellipse = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (100, 100))
     badregions = cv2.morphologyEx(badregions, cv2.MORPH_OPEN,  ellipse, borderType=cv2.BORDER_REPLICATE)
+    if showdebugplots:
+      print("after big open")
+      plt.imshow(badregions.get())
+      plt.show()
     badregions = cv2.morphologyEx(badregions, cv2.MORPH_CLOSE, ellipse, borderType=cv2.BORDER_REPLICATE)
+    if showdebugplots:
+      print("after big close")
+      plt.imshow(badregions.get())
+      plt.show()
 
     if dilatesize is not None:
       ellipse = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (dilatesize, dilatesize))
