f2670556e7b869c5f1802a0b25759b622e649920
Heshy Roskes <heshyr@gmail.com>
2020-08-11 11:25:49 -0400

    plot isotropy vs. stitching ratio

--- alignment/isotropy.py
+++ alignment/isotropy.py
@@ -2,22 +2,44 @@ import numpy as np
 from ..utilities import units
 from .plots import plotpairwisealignments, plt
 
-def isotropy(alignmentset, **kwargs):
-  vectors = plotpairwisealignments(alignmentset, showplot=False, saveas=None, pull=False, pixelsormicrons="pixels", **kwargs)
-  maxfreq = 5
-  bins = 48
-  cosintegrals = {i: 0 for i in range(maxfreq)}
-  sinintegrals = {i: 0 for i in range(maxfreq)}
-  plt.hist([np.arctan2(*units.nominal_values(v)) for v in vectors], bins=bins)
+def isotropy(alignmentset, maxfreq=5, bins=24, showplot=True, **kwargs):
+  vectors = plotpairwisealignments(alignmentset, showplot=False, saveas=None, pull=False, errorbars=False, pixelsormicrons="pixels", **kwargs)
+  cosintegrals = np.zeros(shape=maxfreq)
+  sinintegrals = np.zeros(shape=maxfreq)
+  rs = []
+  phis = []
   for v in vectors:
-    r = 1#units.nominal_values(sum(v**2))
-    phi = np.arctan2(*units.nominal_values(v))
+    rs.append(1)
+    #rs.append(sum(v**2))
+    phis.append(np.arctan2(*v))
+  plt.hist(phis, weights=rs, bins=bins)
+  for r, phi in zip(rs, phis):
     for i in range(maxfreq):
-      cosintegrals[i] += r * np.cos(i*phi) / len(vectors) / np.pi
-      sinintegrals[i] += r * np.sin(i*phi) / len(vectors) / np.pi
+      cosintegrals[i] += r * np.cos(i*phi) / np.pi
+      sinintegrals[i] += r * np.sin(i*phi) / np.pi
   x = np.linspace(-np.pi, np.pi, 1001)
   y = 0*x
   for i in range(maxfreq):
     print(f"{i:3d}  {cosintegrals[i]:.3f}  {sinintegrals[i]:.3f}")
-    y += len(vectors) * (cosintegrals[i] * np.cos(i*x) + sinintegrals[i] * np.sin(i*x)) / (2 if i==0 else 1) * (2*np.pi / bins)
+    y += (cosintegrals[i] * np.cos(i*x) + sinintegrals[i] * np.sin(i*x)) / (2 if i==0 else 1) * (2*np.pi / bins)
     plt.plot(x, y)
+
+  if showplot:
+    plt.show()
+  else:
+    plt.close()
+
+  return cosintegrals, sinintegrals
+
+def stitchingisotropy(alignmentset, cornerfractions=np.linspace(0, 1, 101)):
+  ampfourier = []
+  for cornerfraction in cornerfractions:
+    result = alignmentset.stitch(saveresult=False, scaleedges=1-cornerfraction, scalecorners=cornerfraction)
+    alignmentset.applystitchresult(result)
+    cosfourier, sinfourier = isotropy(alignmentset, stitched=True, showplot=False)
+    ampfourier.append((cosfourier**2 + sinfourier**2) ** .5)
+  ampfourier = np.array(ampfourier)
+  for amps in ampfourier.T:
+    plt.scatter(cornerfractions, amps)
+  plt.show()
+  return ampfourier
