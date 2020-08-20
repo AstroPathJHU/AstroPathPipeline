3590d983054338dee5ced4f6123a6990de86428f
Heshy Roskes <heshyr@gmail.com>
2020-08-20 14:28:24 -0400

    more efficient if you want to keeprawimages

--- alignment/alignmentset.py
+++ alignment/alignmentset.py
@@ -77,11 +77,14 @@ class AlignmentSetBase(FlatwSampleBase, RectangleOverlapCollection):
     self.logger.info("finished align loop for "+self.SlideID)
     return sum_mse
 
-  def getDAPI(self, keeprawimages=False, mean_image=None, overwrite=True):
+  def getDAPI(self, keeprawimages=False, mean_image=None):
     self.logger.info("getDAPI")
     with contextlib.ExitStack() as stack:
       for r in self.rectangles:
         stack.enter_context(r.using_original_image())
+        if keeprawimages:
+          for r in self.rectangles:
+            r.originalimage
       self.images = [r.image for r in self.rectangles]
 
     #create the dictionary of compiled GPU FFT objects if possible
