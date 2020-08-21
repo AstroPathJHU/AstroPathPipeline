exposure time runs but gives the wrong result

alignment/alignmentset.py
@@ -3,6 +3,7 @@
 import contextlib, cv2, methodtools, numpy as np, traceback
 
 from ..baseclasses.overlap import RectangleOverlapCollection
+from ..baseclasses.rectangle import RectangleProvideImage
 from ..baseclasses.sample import FlatwSampleBase, ReadRectangles, ReadRectanglesFromXML
 from ..utilities import units
 from ..utilities.tableio import readtable, writetable
@@ -111,21 +112,36 @@ class AlignmentSetBase(FlatwSampleBase, RectangleOverlapCollection):
     if correct_with_meanimage and recalculate_meanimage :
       self.updateRectangleImages(imgs,usewarpedimages)
       self.meanimage = meanimage([r.image for r in self.rectangles], logger=self.logger)
-    #replace the image in every rectangle
-    for img in imgs :
+
+    for img in imgs:
       if usewarpedimages :
         thisupdateimg=img.warped_image
       else :
         thisupdateimg=img.raw_image
-      #optionally divide by the mean image flatfield
-      if correct_with_meanimage :
-        thisupdateimg=(np.rint(thisupdateimg/self.meanimage.flatfield)).astype(thisupdateimg.dtype)
+
       if img.rectangle_list_index!=-1 : #if the image comes with its index in the list of rectangles it can be directly updated
-        np.copyto(self.rectangles[img.rectangle_list_index].image,thisupdateimg,casting='no')
+        i = img.rectangle_list_index
       else : #otherwise all the rectangles have to be searched
-        thisrect = [r for r in self.rectangles if img.rawfile_key==r.file.rstrip('.im3')]
-        assert len(thisrect)==1 
-        np.copyto(thisrect[0].image,thisupdateimg,casting='no')
+        i = [i for (i, r) in enumerate(self.rectangles) if img.rawfile_key==r.file.rstrip('.im3')]
+        assert len(i)==1
+        i = i[0]
+
+      r = self.rectangles[i]
+      newr = RectangleProvideImage(rectangle=r, image=thisupdateimg, readingfromfile=False)
+      self.rectangles[i] = newr
+
+      mean_image = None
+      if not recalculate_meanimage:
+        mean_image = r.meanimage
+
+      newr = AlignmentRectangle(originalrectangle=r, mean_image=mean_image, use_mean_image=correct_with_meanimage)
+      self.rectangles[i] = newr
+
+    for r in self.rectangles:
+      r.setrectanglelist(self.rectangles)
+
+    for o in self.overlaps:
+      o.updaterectangles(self.rectangles)
 
   def getOverlapComparisonImagesDict(self) :
     """

alignment/rectangle.py
@@ -5,10 +5,14 @@ from ..utilities.misc import dummylogger
 from .flatfield import meanimage
 
 class AlignmentRectangle(RectangleTransformImageBase):
-  def __init__(self, *args, mean_image=None, keepraw=False, logger=dummylogger, **kwargs):
-    super().__init__(originalrectangle=RectangleWithImage(*args, **kwargs))
+  def __init__(self, *args, mean_image=None, keepraw=False, use_mean_image=True, logger=dummylogger, originalrectangle=None, **kwargs):
+    if originalrectangle is not None:
+      super().__init__(*args, originalrectangle=originalrectangle, **kwargs)
+    else:
+      super().__init__(originalrectangle=RectangleWithImage(*args, **kwargs))
     self.__allrectangles = None
     self.__meanimage = mean_image
+    self.__usemeanimage = use_mean_image
     self.__rawimage = None
     self.__keeprawimage = False
     self.__logger = logger
@@ -17,12 +21,13 @@ class AlignmentRectangle(RectangleTransformImageBase):
     self.__allrectangles = allrectangles
 
   def transformimage(self, originalimage):
+    if not self.__usemeanimage: return originalimage
+
     self.__setmeanimage()
     for r in self.__allrectangles:
       r.__setmeanimage(mean_image=self.__meanimage)
     img = np.empty_like(originalimage)
     img[:] = np.rint(originalimage / self.__meanimage.flatfield)
-    self.__meanimage = None
     return img
 
   def __setmeanimage(self, mean_image=None):
@@ -43,3 +48,7 @@ class AlignmentRectangle(RectangleTransformImageBase):
   @property
   def layer(self):
     return self.originalrectangle.layer
+
+  @property
+  def meanimage(self):
+    return self.__meanimage

exposuretime/exposure_time_fit.py
@@ -130,7 +130,7 @@ class SingleLayerExposureTimeFit :
         update_images = []
         for ri,r in enumerate(a.rectangles) :
             rfkey=r.file.rstrip('.im3')
-            image = np.rint((r.image*a.meanimage.flatfield)/self.flatfield).astype(np.uint16)
+            image = np.rint(r.rawimage/self.flatfield).astype(np.uint16)
             image = smoothImageWorker(image,smoothsigma)
             update_images.append(UpdateImage(rfkey,image,ri))
         #update and align with the smoothed images
@@ -169,7 +169,7 @@ class SingleLayerExposureTimeFit :
 
     #helper function to return a list of overlap ns for overlaps where the p1 and p2 image exposure times are different
     def __getOverlapsWithExposureTimeDifferences(self,exp_times) :
-        a = AlignmentSetFromXML(self.metadata_top_dir,self.rawfile_top_dir,self.sample,nclip=CONST.N_CLIP,readlayerfile=False,layer=self.layer)
+        a = AlignmentSetFromXML(self.metadata_top_dir,self.rawfile_top_dir,self.sample,nclip=CONST.N_CLIP,readlayerfile=False,layer=self.layer,filetype="raw")
         olaps_with_et_diffs = []
         for olap in a.overlaps :
             p1key = (([r for r in a.rectangles if r.n==olap.p1])[0].file).rstrip('.im3')
@@ -247,13 +247,13 @@ class SingleLayerExposureTimeFit :
         #make an alignmentset for just those overlaps and correct the raw images
         et_fit_logger.info(f'Making an AlignmentSet for {len(raw_olap_ns_for_plots)} pre/postfit overlay images for layer {self.layer}')
         a = AlignmentSetFromXML(self.metadata_top_dir,self.rawfile_top_dir,self.sample,selectoverlaps=raw_olap_ns_for_plots,
-                                onlyrectanglesinoverlaps=True,nclip=CONST.N_CLIP,readlayerfile=False,layer=self.layer)
+                                onlyrectanglesinoverlaps=True,nclip=CONST.N_CLIP,readlayerfile=False,layer=self.layer,filetype="raw")
         et_fit_logger.info(f'Correcting images for plots in layer {self.layer}')
-        a.getDAPI(filetype='raw')
+        a.getDAPI()
         raw_update_images = []
         for ri,r in enumerate(a.rectangles) :
             rfkey=r.file.rstrip('.im3')
-            image = np.rint((r.image*a.meanimage.flatfield)/self.flatfield).astype(np.uint16)
+            image = np.rint(r.rawimage/self.flatfield).astype(np.uint16)
             raw_update_images.append(UpdateImage(rfkey,copy.deepcopy(image),ri))
         raw_olap_images = {}
         et_fit_logger.info(f'Updating rectangle images and aligning overlaps for plots in layer {self.layer}')