65402d7ba22c22787511ab0c56668b1e70a9e046
Maggie Eminizer <margaret.eminizer@gmail.com>
2020-08-20 21:55:35 -0400

    bugfixes for run_many_fits_with_pool

--- warping/run_many_fits_with_pool.py
+++ warping/run_many_fits_with_pool.py
@@ -208,18 +208,10 @@ if __name__=='__main__' :
         writetable(f'{os.path.basename(os.path.normpath(args.workingdir_name))}_weighted_average_{CONST.FIT_RESULT_CSV_FILE_NAME}',[w_avg_result])
         w_avg_warp.writeOutWarpFields(os.path.basename(os.path.normpath(args.workingdir_name)))
     #aggregate the different metadata summary and field log files into one
-    all_metadata_summaries = []; all_field_logs = []
+    all_field_logs = []
     for dirname in dirnames :
-        all_metadata_summaries.append((readtable(os.path.join(dirname,f'metadata_summary_{os.path.basename(os.path.normpath(dirname))}.csv'),MetadataSummary))[0])
         all_field_logs+=((readtable(os.path.join(dirname,f'field_log_{os.path.basename(os.path.normpath(dirname))}.csv'),FieldLog)))
-    aggregated_mdss = []
-    for sn in set([mds.sample_name for mds in all_metadata_summaries]) :
-        this_samp_mdss = [mds for mds in all_metadata_summaries if mds.sample_name==sn]
-        new_mds = MetadataSummary(sn,this_samp_mdss[0].project,this_samp_mdss[0].cohort,this_samp_mdss[0].microscope_name,
-                                  min([mds.mindate for mds in this_samp_mdss]),max([mds.maxdate for mds in this_samp_mdss]))
-        aggregated_mdss.append(new_mds)
     with cd(args.workingdir_name) :
-        writetable(f'metadata_summary_{os.path.basename(os.path.normpath(args.workingdir_name))}.csv',aggregated_mdss)
         writetable(f'field_log_{os.path.basename(os.path.normpath(args.workingdir_name))}.csv',all_field_logs)
     warp_logger.info('Done.')
 
