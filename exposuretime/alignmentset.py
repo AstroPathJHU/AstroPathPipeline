16a0117066cb4d98c8d80dc323cb0eb90054a553
Heshy Roskes <heshyr@gmail.com>
2020-08-31 12:03:46 -0400

    move to new rectangle setup

--- exposuretime/alignmentset.py
+++ exposuretime/alignmentset.py
@@ -1,18 +1,29 @@
 import numpy as np
 from ..alignment.alignmentset import AlignmentSetFromXML
 from ..alignment.rectangle import AlignmentRectangle
+from ..baseclasses.rectangle import RectangleTransformationBase
 from ..utilities.img_file_io import smoothImageWorker
 
-class RectangleForExposureTime(AlignmentRectangle):
-  def __init__(self, *args, flatfield, smoothsigma, **kwargs):
+class ApplyFlatfield(RectangleTransformationBase):
+  def __init__(self, flatfield):
     self.__flatfield = flatfield
+  def transform(self, originalimage):
+    return np.rint(originalimage / self.__flatfield).astype(np.uint16)
+
+class SmoothImage(RectangleTransformationBase):
+  def __init__(self, smoothsigma):
     self.__smoothsigma = smoothsigma
-    super().__init__(*args, **kwargs)
-  def transformimage(self, originalimage):
-    image = np.rint(originalimage / self.__flatfield).astype(np.uint16)
-    if self.__smoothsigma is not None:
-      image = smoothImageWorker(image,self.__smoothsigma)
-    return image
+  def transform(self, originalimage):
+    return smoothImageWorker(originalimage, self.__smoothsigma)
+
+class RectangleForExposureTime(AlignmentRectangle):
+  def __init__(self, *args, flatfield, smoothsigma, transformations=None, **kwargs):
+    if transformations is None: transformations = []
+    if flatfield is not None:
+      transformations.append(ApplyFlatfield(flatfield))
+    if smoothsigma is not None:
+      transformations.append(SmoothImage(smoothsigma))
+    super().__init__(*args, transformations=transformations, **kwargs)
 
 class AlignmentSetForExposureTime(AlignmentSetFromXML):
   def __init__(self, *args, flatfield, smoothsigma, **kwargs):
