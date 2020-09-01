7a7dac9f49c4b39a8e7fd24b3826189d2aa51d3c
Maggie Eminizer <margaret.eminizer@gmail.com>
2020-09-01 14:57:31 -0400

    4-space indent instead

--- exposuretime/alignmentset.py
+++ exposuretime/alignmentset.py
@@ -5,38 +5,38 @@ from ..baseclasses.rectangle import RectangleTransformationBase
 from ..utilities.img_file_io import smoothImageWorker
 
 class ApplyFlatfield(RectangleTransformationBase):
-  def __init__(self, flatfield):
-    self.__flatfield = flatfield
-  def transform(self, originalimage):
-    return np.rint(originalimage / self.__flatfield).astype(np.uint16)
+    def __init__(self, flatfield):
+        self.__flatfield = flatfield
+    def transform(self, originalimage):
+        return np.rint(originalimage / self.__flatfield).astype(np.uint16)
 
 class SmoothImage(RectangleTransformationBase):
-  def __init__(self, smoothsigma):
-    self.__smoothsigma = smoothsigma
-  def transform(self, originalimage):
-    return smoothImageWorker(originalimage, self.__smoothsigma)
+    def __init__(self, smoothsigma):
+        self.__smoothsigma = smoothsigma
+    def transform(self, originalimage):
+        return smoothImageWorker(originalimage, self.__smoothsigma)
 
 class RectangleForExposureTime(AlignmentRectangle):
-  def __init__(self, *args, flatfield, smoothsigma, transformations=None, **kwargs):
-    if transformations is None: transformations = []
-    if flatfield is not None:
-      transformations.append(ApplyFlatfield(flatfield))
-    if smoothsigma is not None:
-      transformations.append(SmoothImage(smoothsigma))
-    super().__init__(*args, transformations=transformations, use_mean_image=False, **kwargs)
+    def __init__(self, *args, flatfield, smoothsigma, transformations=None, **kwargs):
+        if transformations is None: transformations = []
+        if flatfield is not None:
+            transformations.append(ApplyFlatfield(flatfield))
+        if smoothsigma is not None:
+            transformations.append(SmoothImage(smoothsigma))
+        super().__init__(*args, transformations=transformations, use_mean_image=False, **kwargs)
 
 class AlignmentSetForExposureTime(AlignmentSetFromXML):
-  def __init__(self, *args, flatfield, smoothsigma, **kwargs):
-    self.__flatfield = flatfield
-    self.__smoothsigma = smoothsigma
-    super().__init__(*args, **kwargs)
+    def __init__(self, *args, flatfield, smoothsigma, **kwargs):
+        self.__flatfield = flatfield
+        self.__smoothsigma = smoothsigma
+        super().__init__(*args, **kwargs)
 
-  rectangletype = RectangleForExposureTime
+    rectangletype = RectangleForExposureTime
 
-  @property
-  def rectangleextrakwargs(self):
-    return {
-      **super().rectangleextrakwargs,
-      "flatfield": self.__flatfield,
-      "smoothsigma": self.__smoothsigma
-    }
+    @property
+    def rectangleextrakwargs(self):
+        return {
+            **super().rectangleextrakwargs,
+            "flatfield": self.__flatfield,
+            "smoothsigma": self.__smoothsigma
+        }
