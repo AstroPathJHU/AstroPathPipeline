e267b9cf28cfd76a4141e4372e1df02d517e2100
Maggie Eminizer <margaret.eminizer@gmail.com>
2020-10-13 14:01:22 -0400

    make/run commands for other fit groups and collect results

--- warping/run_fits_for_sample.py
+++ warping/run_fits_for_sample.py
@@ -3,8 +3,9 @@ from .utilities import warp_logger, WarpingError, WarpFitResult, addCommonWarpin
 from .config import CONST
 from ..utilities.tableio import readtable, writetable
 from ..utilities.misc import cd
+import numpy as np
 from argparse import ArgumentParser
-import os, subprocess, random
+import os, subprocess, random, shutil
 
 #################### FILE-SCOPE CONSTANTS ####################
 INITIAL_PATTERN_DIR_STEM = 'warping_initial_pattern'
@@ -91,7 +92,7 @@ def getInitialPatternFitCmd(wdn,args) :
     return cmd
 
 #helper function to make the command to run for the principal point fit group
-def getPrincipalPointFitCmd(wdn,args,init_k1,init_k2,init_k3) :
+def getPrincipalPointFitCmd(wdn,args,k1,k2,k3) :
     #start with the call to run_many_fits_with_pool
     cmd = RUN_MANY_FITS_CMD_BASE
     #add the positional arguments
@@ -119,13 +120,44 @@ def getPrincipalPointFitCmd(wdn,args,init_k1,init_k2,init_k3) :
     #fix the focal lengths and warping parameters
     cmd+='--fixed fx,fy,k1,k2,k3,p1,p2 '
     #add the initial radial warping parameters from the weighted avg. of the last group of fits
-    cmd+=f'--init_pars k1={init_k1},k2={init_k2},k3={init_k3}'
+    cmd+=f'--init_pars k1={k1},k2={k2},k3={k3}'
     #return the command
     return cmd
 
 #helper function to make the command to run for the final pattern fit group
-def getFinalPatternFitCmd(wdn,args) :
-    pass
+def getFinalPatternFitCmd(wdn,args,k1,k2,k3,cx,cx_err,cy,cy_err) :
+    #start with the call to run_many_fits_with_pool
+    cmd = RUN_MANY_FITS_CMD_BASE
+    #add the positional arguments
+    argvars = vars(args)
+    for ppan in POSITIONAL_PASSTHROUGH_ARG_NAMES :
+        cmd+=f' {argvars[ppan]} '
+    #add the working directory argument
+    this_job_dir_path = os.path.abspath(os.path.join(args.workingdir,wdn))
+    cmd+=f'{this_job_dir_path} '
+    #add the number of jobs positional argument
+    cmd+=f'{args.final_pattern_octets} '
+    #add the passthrough arguments and flags
+    for pan in PASSTHROUGH_ARG_NAMES :
+        if argvars[pan] is not None :
+            cmd+=f'--{pan} {argvars[pan]} '
+    for pfn in PASSTHROUGH_FLAG_NAMES :
+        if argvars[pfn] :
+            cmd+=f'--{pfn} '
+    #add the number of iterations to run
+    cmd+=f'--max_iters {args.final_pattern_max_iters} '
+    #the octets are in the working directory
+    cmd+=f'--octet_run_dir {this_job_dir_path} '
+    #select the first single octet for every job since we've already split up the octets for the sample
+    cmd+='--octet_selection first_1 '
+    #fix the focal lengths and tangential warping parameters
+    cmd+='--fixed fx,fy,p1,p2 '
+    #add the initial radial warping parameters from the weighted avg. of the last group of fits
+    cmd+=f'--init_pars cx={cx},cy={cy},k1={k1},k2={k2},k3={k3} '
+    #add the bounds on the center point
+    cmd+=f'--init_bounds cx={cx-cx_err}:{cx+cx_err},cy={cy-cy_err}:{cy+cy_err}'
+    #return the command
+    return cmd
 
 #################### MAIN SCRIPT ####################
 
@@ -160,19 +192,53 @@ if __name__=='__main__' :
     #set up the three directories with their octet groupings
     dirname_1, dirname_2, dirname_3 = setUpFitDirectories(args)
     if args.mode!='find_octets' :
+        #start up the file that will have the commands written into it
+        cmd_file_path = os.path.abspath(os.path.join(args.workingdir,'run_many_fit_commands.txt'))
         #get the command for the initial pattern fits and run it
         cmd_1 = getInitialPatternFitCmd(dirname_1,args)
+        with open(cmd_file_path,'w') as fp :
+            fp.write(f'{cmd_1}\n\n')
         subprocess.call(cmd_1)
     if args.mode=='fit' :
         #figure out the radial warping parameters to use when finding the principal point
-        w_avg_res_1 = readtable(os.path.abspath(os.path.join(args.workingdir,dirname_1,f'{dirname_1}_weighted_average_{CONST.FIT_RESULT_CSV_FILE_NAME}')))
+        w_avg_res_1 = (readtable(os.path.abspath(os.path.join(args.workingdir,dirname_1,
+                                                            f'{dirname_1}_weighted_average_{CONST.FIT_RESULT_CSV_FILE_NAME}')),WarpFitResult))[0]
+        init_k1 = w_avg_res_1.k1; init_k2 = w_avg_res_1.k2; init_k3 = w_avg_res_1.k3
         #get the command for the central principal point fits and run it
-        cmd_2 = getPrincipalPointFitCmd(dirname_2,args,w_avg_res_1.k1,w_avg_res_1.k2,w_avg_res_1.k3)
+        cmd_2 = getPrincipalPointFitCmd(dirname_2,args,init_k1,init_k2,init_k3)
+        with open(cmd_file_path,'a') as fp :
+            fp.write(f'{cmd_2}\n\n')
         subprocess.call(cmd_2)
+        #find the weighted mean of the principal point from the previous run results 
+        all_results_2 = readtable(os.path.abspath(os.path.join(args.workingdir,dirname_2,f'all_results_{dirname_2}.csv')),WarpFitResult)
+        w_cx = 0.; w_cy = 0.; sw = 0.; sw2 = 0.
+        for r in all_results_2 :
+            w = r.cost_reduction
+            if w<0 :
+                continue
+            w_cx+=(w*r.cx); w_cy=(w*r.cy); sw+=w; sw2+=w**2
+        w_cx/=sw; w_cy/=sw
+        w_cx_e = np.sqrt(((np.std([r.cx for r in all_results_2 if r.cost_reduction>0])**2)*sw2)/(sw**2))
+        w_cy_e = np.sqrt(((np.std([r.cx for r in all_results_2 if r.cost_reduction>0])**2)*sw2)/(sw**2))
         #get the command for the final pattern fits and run it
-        cmd_3 = getFinalPatternFitCmd(dirname_3,args)
+        cmd_3 = getFinalPatternFitCmd(dirname_3,args,init_k1,init_k2,init_k3,w_cx,w_cx_e,w_cy,w_cy_e)
+        with open(cmd_file_path,'a') as fp :
+            fp.write(f'{cmd_3}\n')
         subprocess.call(cmd_3)
         warp_logger.info(f'All fits for {args.sample} warping pattern completed')
+        #move and rename the final warping field and weighted average result files from the last fit
+        old_w_avg_fit_result_fp = os.path.abspath(os.path.join(args.workingdir,dirname_3,f'{dirname_3}_weighted_average_{CONST.FIT_RESULT_CSV_FILE_NAME}'))
+        new_w_avg_fit_result_fp = os.path.abspath(os.path.join(args.workingdir,
+                                                f'{os.path.basename(os.path.normpath(args.workingdir))}_weighted_average_{CONST.FIT_RESULT_CSV_FILE_NAME}'))
+        shutil.move(old_w_avg_fit_result_fp,new_w_avg_fit_result_fp)
+        old_w_avg_dx_wf_fp = os.path.abspath(os.path.join(args.workingdir,dirname_3,f'{CONST.X_WARP_BIN_FILENAME}_{dirname_3}.bin'))
+        old_w_avg_dy_wf_fp = os.path.abspath(os.path.join(args.workingdir,dirname_3,f'{CONST.Y_WARP_BIN_FILENAME}_{dirname_3}.bin'))
+        new_w_avg_dx_wf_fp = os.path.abspath(os.path.join(args.workingdir,
+                                                f'{CONST.X_WARP_BIN_FILENAME}_{os.path.basename(os.path.normpath(args.workingdir))}.bin'))
+        new_w_avg_dy_wf_fp = os.path.abspath(os.path.join(args.workingdir,
+                                                f'{CONST.Y_WARP_BIN_FILENAME}_{os.path.basename(os.path.normpath(args.workingdir))}.bin'))
+        shutil.move(old_w_avg_dx_wf_fp,new_w_avg_dx_wf_fp)
+        shutil.move(old_w_avg_dy_wf_fp,new_w_avg_dy_wf_fp)
     warp_logger.info(f'Done.')
 
     
