9dba25d7243e5d868832d56e1a5e3eb9516d6130
Maggie Eminizer <margaret.eminizer@gmail.com>
2020-10-27 14:57:15 -0400

    move logging around

--- correct_and_copy_rawfiles/rawfile_corrector.py
+++ correct_and_copy_rawfiles/rawfile_corrector.py
@@ -38,11 +38,17 @@ class RawfileCorrector :
         self._metadata_top_dir = args.metadata_top_dir
         #set the sample name
         self._sample_name = args.sample
+        #start up the logfile
+        self._startUpLogFile(args.logfile_name_stem)
         #get the image dimensions and layer argument
         self._img_dims = getImageHWLFromXMLFile(self._metadata_top_dir,self._sample_name)
         if args.layer!=-1 and (args.layer<1 or args.layer>self._img_dims[2]) :
             raise ValueError(f'ERROR: layer argument {args.layer} is not compatible with image dimensions {self._img_dims}!')
         self._layer = args.layer
+        if self._layer==-1 :
+            self.__writeLog(f'Corrected {self._img_dims[-1]}-layer files will be written out to {self._working_dir_path}')
+        else :
+            self.__writeLog(f'Corrected layer {self._layer} files will be written out to {self._working_dir_path}.')
         #set the working directory path
         self._working_dir_path = args.workingdir
         #see which layer(s) will be run
@@ -53,9 +59,6 @@ class RawfileCorrector :
         self._setFlatfieldVariable(args.skip_flatfielding,args.flatfield_file,layers_to_run)
         #set the warping correction variables
         self._setWarpingVariables(args.skip_warping,args.warp_def,args.warp_shift_file,args.warp_shift,args.warping_scalefactor,layers_to_run)
-        #start up the logfile and add some information to it
-        self._startUpLogFile(args.logfile_name_stem,args.exposure_time_offset_file,args.flatfield_file,args.warp_def,args.warp_shift_file,
-                             args.warp_shift,args.warping_scalefactor)
         #set a couple more instance variables
         self._infile_ext = args.input_file_extension
         self._outfile_ext = f'{args.output_file_extension}'
@@ -73,7 +76,12 @@ class RawfileCorrector :
             all_rawfile_paths = [os.path.join(self._rawfile_top_dir,self._sample_name,fn) for fn in glob.glob(f'*{self._infile_ext}')]
         self.__writeLog(f'Found {len(all_rawfile_paths)} total raw files in {os.path.join(self._rawfile_top_dir,self._sample_name)}')
         if self._max_files!=-1 :
-            all_rawfile_paths=all_rawfile_paths[:self._max_files]
+            if self._max_files>len(all_rawfile_paths) :
+                msg = f'WARNING: only {len(all_rawfile_paths)} were found for {self._sample_name}, but {self._max_files} were requested;'
+                msg+=f' all {len(all_rawfile_paths)} files will be run for this sample instead.'
+                self.__writeLog(msg)
+            else :
+                all_rawfile_paths=all_rawfile_paths[:self._max_files]
             msg = f'Will correct and write out {len(all_rawfile_paths)} file'
             if self._layer==-1 :
                 msg+='s'
@@ -96,10 +104,22 @@ class RawfileCorrector :
             with open(self._logfile_name,'a') as fp :
                 fp.write(f'{line}\n')
 
+    #helper function to start up the log file for the corrector
+    def _startUpLogFile(self,lf_name_stem) :
+        self._logfile_name = f'{lf_name_stem}_{time.strftime("%Y_%m_%d-%H_%M_%S")}.log'
+        with cd(self._working_dir_path) :
+            with open(self._logfile_name,'w') as fp :
+                fp.write('LOGFILE for correct_and_copy_rawfiles\n')
+                fp.write('-------------------------------------\n\n')
+        msg = f'Working directory {os.path.basename(os.path.normpath(self._working_dir_path))} has been created'
+        msg+= f' in {os.path.dirname(os.path.normpath(self._working_dir_path))}.'
+        self.__writeLog(msg)
+
     #helper function to set the exposure time correction variables
     def _setExposureTimeVariables(self,skip_etc,eto_file) :
         if skip_etc :
             self._med_exp_time = None; self._et_correction_offset = None
+            self.__writeLog('Corrections for exposure time WILL NOT be applied.')
             return
         if self._layer==-1 :
             self._med_exp_time, self._et_correction_offset = getMedianExposureTimesAndCorrectionOffsetsForSample(self._metadata_top_dir,
@@ -117,11 +137,20 @@ class RawfileCorrector :
                 os.mkdir(APPLIED_CORRECTION_PLOT_DIR_NAME)
             with cd(APPLIED_CORRECTION_PLOT_DIR_NAME) :
                 writetable('applied_exposure_time_correction_offsets.csv',los)
+        self.__writeLog(f'Exposure time corrections WILL be applied based on offset factors in {eto_file}')
+        if self._layer==-1 :
+            for ln in range(1,self._img_dims[-1]+1) :
+                msg = f'(Layer {ln} median sample exposure time={self._med_exp_time[ln-1]};'
+                msg+= f' exposure time correction offset = {self._et_correction_offset[ln-1]})'
+                self.__writeLog(msg)
+        else :
+            self.__writeLog(f'(Median sample exposure time={self._med_exp_time}; exposure time correction offset = {self._et_correction_offset})')
 
     #helper function to set the flatfield correction variable
     def _setFlatfieldVariable(self,skip_ff,ff_file,layers_to_run) :
         if skip_ff :
             self._ff = None
+            self.__writeLog('Flatfielding corrections WILL NOT be applied.')
             return
         self._ff = getRawAsHWL(ff_file,*(self._img_dims),dtype=FF_CONST.IMG_DTYPE_OUT)
         if self._layer!=-1 :
@@ -142,17 +171,21 @@ class RawfileCorrector :
                     plt.savefig(savename)
                     plt.close()
                     cropAndOverwriteImage(savename)
+        self.__writeLog(f'Flatfield corrections WILL be applied as read from {ff_filepath}')
 
     #helper function to set the warping variables
     def _setWarpingVariables(self,skip_w,w_def,ws_file,arg_ws,w_sf,layers_to_run) :
         if skip_w :
             self._warps = None; self._dx_warp_field = None; self._dy_warp_field = None
+            self.__writeLog('Warping corrections WILL NOT be applied.')
             return
+        msg='Warping corrections will be applied as read from '
         with cd(self._working_dir_path) :
             if not os.path.isdir(APPLIED_CORRECTION_PLOT_DIR_NAME) :
                 os.mkdir(APPLIED_CORRECTION_PLOT_DIR_NAME)
         #first try to define the warping from a parameter file
         if w_def.endswith('.csv') :
+            msg+=f'{w_def}'
             warp_fit_result = readtable(w_def,WarpingSummary)
             if len(warp_fit_result)>1 :
                 raise ValueError(f'ERROR: warp fit result file {w_def} has more than one set of parameters!')
@@ -163,11 +196,13 @@ class RawfileCorrector :
                     warp_shifts = readtable(ws_file,WarpShift)
                 except Exception as e :
                     raise ValueError(f'ERROR: file {ws_file} is not recognized as a set of WarpShift objects! Exception: {e}')
+                msg+=f' and shifted as read from {ws_file}'
             elif arg_ws is not None :
                 cx_shift,cy_shift = arg_ws.split(',')
                 cx_shift = float(cx_shift); cy_shift = float(cy_shift)
                 for ln in layers_to_run :
                     warp_shifts.append(WarpShift(ln,cx_shift,cy_shift))
+                msg+=f' and shifted by {arg_ws}'
             if self._layer!=-1 :
                 warp_shifts = [ws for ws in warp_shifts if ws.layer_n==self._layer]
             if len(warp_shifts)>0 :
@@ -193,6 +228,7 @@ class RawfileCorrector :
                 raise FileNotFoundError(f'ERROR: dy warp field path {dy_warp_field_path} does not exist!')
             self._dx_warp_field = (w_sf)*(getRawAsHW(dx_warp_field_path,*(self._img_dims[:-1]),dtype=WARP_CONST.OUTPUT_FIELD_DTYPE))
             self._dy_warp_field = (w_sf)*(getRawAsHW(dy_warp_field_path,*(self._img_dims[:-1]),dtype=WARP_CONST.OUTPUT_FIELD_DTYPE))
+            msg+=f'{dx_warp_field_path} and {dy_warp_field_path}'
             r_warp_field = np.sqrt((self._dx_warp_field**2)+(self._dy_warp_field**2))
             with cd(os.path.join(self._working_dir_path,APPLIED_CORRECTION_PLOT_DIR_NAME)) :
                 f,ax = plt.subplots(1,3,figsize=(3*6.4,(self._img_dims[0]/self._img_dims[1])*6.4))
@@ -209,52 +245,9 @@ class RawfileCorrector :
                 plt.savefig(savename)
                 plt.close()
                 cropAndOverwriteImage(savename)
-
-    #helper function to start up the log file and add some information to it
-    def _startUpLogFile(self,lf_name_stem,eto_filepath,ff_filepath,w_def,ws_file,arg_ws,w_sf) :
-        self._logfile_name = f'{lf_name_stem}_{time.strftime("%Y_%m_%d-%H_%M_%S")}.log'
-        with cd(self._working_dir_path) :
-            with open(self._logfile_name,'w') as fp :
-                fp.write('LOGFILE for correct_and_copy_rawfiles\n')
-                fp.write('-------------------------------------\n\n')
-        msg = f'Working directory {os.path.basename(os.path.normpath(self._working_dir_path))} has been created'
-        msg+= f' in {os.path.dirname(os.path.normpath(self._working_dir_path))}.'
-        self.__writeLog(msg)
-        if self._layer==-1 :
-            self.__writeLog(f'Corrected {self._img_dims[-1]}-layer files will be written out to {self._working_dir_path}')
-        else :
-            self.__writeLog(f'Corrected layer {self._layer} files will be written out to {self._working_dir_path}.')
-        if self._med_exp_time is None and self._et_correction_offset is None :
-            self.__writeLog('Corrections for exposure time WILL NOT be applied.')
-        else :
-            self.__writeLog(f'Exposure time corrections WILL be applied based on offset factors in {eto_filepath}')
-            if self._layer==-1 :
-                for ln in range(1,self._img_dims[-1]+1) :
-                    msg = f'(Layer {ln} median sample exposure time={self._med_exp_time[ln-1]};'
-                    msg+= f' exposure time correction offset = {self._et_correction_offset[ln-1]})'
-                    self.__writeLog(msg)
-            else :
-                self.__writeLog(f'(Median sample exposure time={self._med_exp_time}; exposure time correction offset = {self._et_correction_offset})')
-        if self._ff is None :
-            self.__writeLog('Flatfielding corrections WILL NOT be applied.')
-        else :
-            self.__writeLog(f'Flatfield corrections WILL be applied as read from {ff_filepath}')
-        if self._warps is None and self._dx_warp_field is None and self._dy_warp_field is None :
-            self.__writeLog('Warping corrections WILL NOT be applied.')
-        else :
-            msg='Warping corrections will be applied as read from '
-            if self._warps is not None :
-                msg+=f'{w_def}'
-                if ws_file is not None :
-                    msg+=f' and shifted as read from {ws_file}'
-                elif arg_ws is not None :
-                    msg+=f' and shifted by {arg_ws}'
-            elif self._dx_warp_field is not None and self._dy_warp_field is not None :
-                dx_warp_field_path, dy_warp_field_path = getWarpFieldPathsFromWarpDef(w_def)
-                msg+=f'{dx_warp_field_path} and {dy_warp_field_path}'
-            if w_sf!=1.0 :
+        if w_sf!=1.0 :
                 msg+=f' and multiplied by {w_sf}'
-            self.__writeLog(msg)
+        self.__writeLog(msg)
 
     #helper function to read, correct, and write out a single image layer
     #can be run in parallel
