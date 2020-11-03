f9952c97001f81dc509709339f33d27199fd6131
Maggie Eminizer <margaret.eminizer@gmail.com>
2020-11-02 20:25:24 -0500

    WIP commit again
    this fully does not work as it is

--- flatfield/logging.py
+++ flatfield/logging.py
@@ -1,4 +1,6 @@
 #imports
+from ..baseclasses.sample import SampleDef
+from ..baseclasses.logging import getlogger
 import logging
 
 class RunLogger :
@@ -16,20 +18,32 @@ class RunLogger :
         self._module = mode
         self._batch_mode = self._module in ('slide_mean_image')
         self._workingdir_path = workingdir_path
-        self._global_logger = self._getGlobalLogger()
+        self._global_logger, self._global_logger_filepath = self._getGlobalLogger()
         self._slide_loggers = {}
 
     def __enter__(self) :
         return self
 
+    def __exit__(self, exc_type, exc_value, exc_traceback) :
+        if exc_value is not None:
+            errmsg = str(exc_value).replace(";", ",")
+            infomsg = repr(traceback.format_exception(exc_type, exc_value, exc_traceback)).replace(";", "")
+            for logger in [self._global_logger]+list(self._slide_loggers.values()) :
+                logger.error(errmsg)
+                logger.info(infomsg)
+                logger.info(f"end {self.module}")
+        return True #Don't ever fully crash (other loggers will handle that if not in batch mode)
+
     #################### PUBLIC FUNCTIONS ####################
 
-    #write an info message to the logger. If slideID is not None, use the relevant SlideLogger instead of the global logger
-    def info(self,msg,slideID=None) :
-        if slideID is None :
+    #write an info message to the logger. If slideID and slide_root are not None, use the relevant SlideLogger instead of the global logger
+    def info(self,msg,slideID=None,slide_root=None) :
+        if slideID is None and slide_root is None :
             self._global_logger.info(msg)
         else :
-            pass
+            if slideID not in self._slide_loggers.keys() :
+                self._addSlideLogger(slideID,slide_root)
+            self._slide_loggers[slideID].info(msg)
 
     #################### PRIVATE HELPER FUNCTIONS ####################
 
@@ -42,8 +56,21 @@ class RunLogger :
         printhandler.setFormatter(formatter)
         printhandler.setLevel(logging.DEBUG)
         logger.addHandler(printhandler)
-        filehandler = logging.FileHandler(os.path.join(workingdir_path,f'global-{self._module}.log'))
+        logger_filepath = os.path.join(workingdir_path,f'global-{self._module}.log')
+        filehandler = logging.FileHandler(logger_filepath)
         filehandler.setFormatter(formatter)
         filehandler.setLevel(logging.INFO)
         logger.addHandler(filehandler)
-        return logger
+        return logger, logger_filepath
+
+    #helper function to add a new single slide logger to the global logger's dictionary
+    def _addSlideLogger(self,slideid,root_dir) :
+        samp = SampleDef(SlideID=slideid,root=root_dir)
+        newlogger = getLogger(module=self._module,root=root_dir,samp=samp,uselogfiles=True,
+                              ########### comment out the two lines below to actually put the logs in the right place ################
+                              mainlog=os.path.join(self._workingdir_path,f'{self._module}.log'),
+                              samplelog=os.path.join(self._workingdir_path,f'{slideid}-{self._module}.log'),
+                              ########### comment out the two lines above to actually put the logs in the right place ################
+                              imagelog=self._global_logger_filepath,reraiseexceptions=(not self._batch_mode))
+        newlogger.critical(self._module)
+        self._slide_loggers[slideid] = newlogger
