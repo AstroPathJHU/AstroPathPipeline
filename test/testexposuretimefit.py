ed8c5ace8deddbaec1c491b064f037422ae70610
Heshy Roskes <heshyr@gmail.com>
2020-11-04 08:32:30 -0500

    bigger rtol

--- test/testexposuretimefit.py
+++ test/testexposuretimefit.py
@@ -37,7 +37,7 @@ fit_group.runFits(None,overlaps_arg,smoothsigma_arg,False,
 new = readtable(working_dir/f"{slideID}_layers_{layers_arg[0]}-{layers_arg[-1]}_best_fit_offsets_exposuretimefit_test_for_jenkins.csv", LayerOffset)
 ref = readtable(folder/"reference"/"exposuretimefit"/f"{slideID}_layers_{layers_arg[0]}-{layers_arg[-1]}_best_fit_offsets_exposuretimefit_test_for_jenkins.csv", LayerOffset)
 for offsetnew, offsetref in zip(new, ref):
-  assertAlmostEqual(offsetnew, offsetref, rtol=1e-5, atol=1e-8)
+  assertAlmostEqual(offsetnew, offsetref, rtol=1e-4, atol=1e-8)
 
 print('Removing working directory...')
 shutil.rmtree(working_dir,ignore_errors=True)
