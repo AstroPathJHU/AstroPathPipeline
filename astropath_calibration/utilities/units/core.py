03b2a1908a03f159a5ab50d328c9d52a0b650d0d
Heshy Roskes <heshyr@gmail.com>
2020-11-19 13:31:53 -0500

    migrate stitch

--- astropath_calibration/utilities/units/core.py
+++ astropath_calibration/utilities/units/core.py
@@ -8,12 +8,17 @@ class Distance:
   def __new__(self, *args, **kwargs):
     return currentmodule.Distance(*args, **kwargs)
 
+def onepixel(pscale):
+  return Distance(pixels=1, pscale=pscale)
+def onemicron(pscale):
+  return Distance(microns=1, pscale=pscale)
+
 class ThingWithPscale:
   @methodtools.lru_cache()
   @property
   def onepixel(self):
-    return Distance(pixels=1, pscale=self.pscale)
+    return onepixel(pscale=self.pscale)
   @methodtools.lru_cache()
   @property
   def onemicron(self):
-    return Distance(microns=1, pscale=self.pscale)
+    return onemicron(pscale=self.pscale)
