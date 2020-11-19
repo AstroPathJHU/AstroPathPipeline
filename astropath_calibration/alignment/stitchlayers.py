674be5a22cf749302e9b3b7ac56d9f0867a0b524
Heshy Roskes <heshyr@gmail.com>
2020-11-19 13:41:40 -0500

    migrate layer alignment

--- astropath_calibration/alignment/stitchlayers.py
+++ astropath_calibration/alignment/stitchlayers.py
@@ -234,8 +234,8 @@ def __stitchlayerscvxpy(*, overlaps, logger=dummylogger):
     x1 = layerx[o.layer1]
     x2 = layerx[o.layer2]
     twonll += cp.quad_form(
-      x1 - x2 + units.pixels(-units.nominal_values(o.result.dxvec), pscale=pscale, power=1),
-      units.pixels(units.np.linalg.inv(o.result.covariance), pscale=pscale, power=-2),
+      x1 - x2 + (-units.nominal_values(o.result.dxvec) / onepixel),
+      units.np.linalg.inv(o.result.covariance) * onepixel**2,
     )
 
   minimize = cp.Minimize(twonll)
@@ -338,8 +338,9 @@ class LayerStitchResult(LayerStitchResultBase):
 
 class LayerStitchResultCvxpy(LayerStitchResultBase):
   def __init__(self, *, x, problem, pscale, **kwargs):
+    onepixel = units.onepixel(pscale=pscale)
     super().__init__(
-      x=units.distances(pixels=x.value, pscale=pscale),
+      x=x.value * onepixel,
       **kwargs
     )
     self.problem = problem
