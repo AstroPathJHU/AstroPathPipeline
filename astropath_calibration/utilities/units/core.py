4fea9398c66cc11341c899fc3a810192b088cbb4
Heshy Roskes <heshyr@gmail.com>
2020-11-19 11:03:56 -0500

    base class for ThingWithPscale

--- astropath_calibration/utilities/units/core.py
+++ astropath_calibration/utilities/units/core.py
@@ -1,1 +1,13 @@
+import methodtools
+
 class UnitsError(Exception): pass
+
+class Distance:
+  def __new__(self, *args, **kwargs):
+    return currentmodule.Distance(*args, **kwargs)
+
+class ThingWithPscale:
+  @methodtools.lru_cache()
+  @property
+  def onepixel(self):
+    return Distance(pixels=1, pscale=self.pscale)
