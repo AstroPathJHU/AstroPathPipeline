19a7a6c975fc88f95296cdf312d9bfbf8e6e0f45
Heshy Roskes <heshyr@gmail.com>
2020-12-17 13:47:42 -0500

    fix zoom cohort

--- astropath_calibration/zoom/zoomcohort.py
+++ astropath_calibration/zoom/zoomcohort.py
@@ -5,8 +5,9 @@ from ..utilities import units
 from .zoom import Zoom
 
 class ZoomCohort(DbloadCohort, FlatwCohort):
-  def __init__(self, *args, zoomroot, **kwargs):
+  def __init__(self, *args, zoomroot, fast=False, **kwargs):
     self.__zoomroot = zoomroot
+    self.__fast = fast
     super().__init__(*args, **kwargs)
 
   sampleclass = Zoom
@@ -16,7 +17,7 @@ class ZoomCohort(DbloadCohort, FlatwCohort):
 
   def runsample(self, sample):
     #sample.logger.info(f"{sample.ntiles} {len(sample.rectangles)}")
-    return sample.zoom()
+    return sample.zoom_wsi(fast=self.__fast)
 
   @property
   def logmodule(self): return "zoom"
@@ -28,21 +29,22 @@ def main(args=None):
   p.add_argument("--debug", action="store_true")
   g = p.add_mutually_exclusive_group()
   g.add_argument("--sampleregex", type=re.compile)
-  g.add_argument("--skip-aligned", action="store_true")
   p.add_argument("--units", choices=("safe", "fast"), default="fast")
   p.add_argument("--dry-run", action="store_true")
+  p.add_argument("--zoom-root", type=pathlib.Path, required=True)
+  p.add_argument("--fast", action="store_true")
   args = p.parse_args(args=args)
 
   units.setup(args.units)
 
-  kwargs = {"root": args.root1, "root2": args.root2}
+  kwargs = {"root": args.root1, "root2": args.root2, "zoomroot": args.zoom_root, "fast": args.fast}
 
   if args.sampleregex is not None:
     kwargs["filter"] = lambda sample: args.sampleregex.match(sample.SlideID)
 
   cohort = ZoomCohort(**kwargs)
   if args.dry_run:
-    print("would align the following samples:")
+    print("would zoom the following samples:")
     for samp in cohort: print(samp)
   else:
     cohort.run()
