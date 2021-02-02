073c415790a4e424a099d480ad46c4232f652634
Heshy Roskes <heshyr@gmail.com>
2021-02-02 10:54:03 -0500

    couple of fixes

--- astropath_calibration/annowarp/annowarpcohort.py
+++ astropath_calibration/annowarp/annowarpcohort.py
@@ -2,13 +2,13 @@ from ..baseclasses.cohort import DbloadCohort, ZoomCohort
 from .annowarpsample import AnnoWarpSample
 
 class AnnoWarpCohortBase(DbloadCohort, ZoomCohort):
-  def __init__(self, *args, tilesize=100, **kwargs):
-    self.tilesize = tilesize
+  def __init__(self, *args, tilepixels=100, **kwargs):
+    self.tilepixels = tilepixels
     super().__init__(*args, **kwargs)
 
   @property
   def initiatesamplekwargs(self):
-    return {**super().initiatesamplekwargs, "tilesize": self.tilesize}
+    return {**super().initiatesamplekwargs, "tilepixels": self.tilepixels}
 
   sampleclass = AnnoWarpSample
 
@@ -18,7 +18,7 @@ class AnnoWarpCohortBase(DbloadCohort, ZoomCohort):
   @classmethod
   def makeargumentparser(cls):
     p = super().makeargumentparser()
-    p.add_argument("--tilesize", type=int, default=100)
+    p.add_argument("--tilepixels", type=int, default=100)
     p.add_argument("--skip-stitched", action="store_true")
     return p
 
@@ -26,18 +26,18 @@ class AnnoWarpCohortBase(DbloadCohort, ZoomCohort):
   def initkwargsfromargumentparser(cls, parsed_args_dict):
     kwargs = {
       **super().initkwargsfromargumentparser(parsed_args_dict),
-      "tilesize": parsed_args_dict.pop("tilesize"),
+      "tilepixels": parsed_args_dict.pop("tilepixels"),
     }
     if parsed_args_dict.pop("skip_stitched"):
       dbloadroot = kwargs["dbloadroot"]
       def isnotstitched(sample):
-        if not (dbloadroot/sample.SlideID/"{sample}_warp-{kwargs['tilesize']}.csv").exists(): return True
-        if not (dbloadroot/sample.SlideID/"{sample}_warp-{kwargs['tilesize']}-stitch.csv").exists(): return True
+        if not (dbloadroot/sample.SlideID/"{sample}_warp-{kwargs['tilepixels']}.csv").exists(): return True
+        if not (dbloadroot/sample.SlideID/"{sample}_warp-{kwargs['tilepixels']}-stitch.csv").exists(): return True
         return False
       kwargs["filters"].append(isnotstitched)
     return kwargs
 
-class AnnoWarpCohort(DbloadCohort, ZoomCohort):
+class AnnoWarpCohort(AnnoWarpCohortBase):
   def runsample(self, sample):
     return sample.runannowarp()
 
