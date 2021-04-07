b09d18b56bb4b2c8c8065b7edeffa3c2371888d7
Maggie Eminizer <margaret.eminizer@gmail.com>
2021-04-07 12:46:47 -0400

    further adjustments (now with the correct layer groups!)

--- astropath/scripts/flagging_hpf_regions.py
+++ astropath/scripts/flagging_hpf_regions.py
@@ -29,27 +29,27 @@ MASK_LAYER_GROUPS           = [(1,9),(10,11),(12,17),(18,20),(21,29),(30,36),(37
 BRIGHTEST_LAYERS           = [5,10,16,19,22,31,41]
 DAPI_LAYER_GROUP_INDEX     = 0
 #RBC_LAYER_GROUP_INDEX      = 1
-RBC_LAYER_GROUP_INDEX      = 2
+RBC_LAYER_GROUP_INDEX      = 4
 FOLD_MIN_PIXELS            = 30000
 FOLD_MIN_SIZE              = 5000
 #FOLD_NLV_CUT               = 0.0035
-FOLD_NLV_CUT               = 0.08
+FOLD_NLV_CUT               = 0.1
 #FOLD_MAX_MEAN              = 0.0030
-FOLD_MAX_MEAN              = 0.07
+FOLD_MAX_MEAN              = 0.085
 #FOLD_MASK_FLAG_CUTS        = [3,3,1,1,0]
 FOLD_MASK_FLAG_CUTS        = [3,0,3,0,1,1,1]
 FOLD_FLAG_STRING           = 'tissue fold or bright dust'
 DUST_MIN_PIXELS            = 30000
 DUST_MIN_SIZE              = 20000
 #DUST_NLV_CUT               = 0.00085
-DUST_NLV_CUT               = 0.03
+DUST_NLV_CUT               = 0.05
 #DUST_MAX_MEAN              = 0.00065
-DUST_MAX_MEAN              = 0.0225
+DUST_MAX_MEAN              = 0.0375
 DUST_STRING                = 'likely dust'
 SATURATION_MIN_PIXELS      = 4500
 SATURATION_MIN_SIZE        = 1000
 #SATURATION_INTENSITY_CUTS  = [100,100,250,400,150]
-SATURATION_INTENSITY_CUTS  = [100,50,75,250,150,100,50]
+SATURATION_INTENSITY_CUTS  = [100,50,150,250,150,100,50]
 SATURATION_FLAG_STRING     = 'saturated likely skin red blood cells or stain'
 
 #logger
@@ -143,7 +143,7 @@ def getImageTissueFoldMask(img_array,exp_times,tissue_mask,exp_t_hists,return_pl
     stacked_fold_masks = np.zeros_like(fold_masks_by_layer_group[0])
     for lgi,layer_group_fold_mask in enumerate(fold_masks_by_layer_group) :
         stacked_fold_masks[layer_group_fold_mask==0]+=10 if lgi in (DAPI_LAYER_GROUP_INDEX,RBC_LAYER_GROUP_INDEX) else 1
-    overall_fold_mask = (np.where(stacked_fold_masks>11,0,1)).astype(np.uint8)
+    overall_fold_mask = (np.where(stacked_fold_masks>12,0,1)).astype(np.uint8)
     #morph and filter the mask using the common operations
     overall_fold_mask = getMorphedAndFilteredMask(overall_fold_mask,tissue_mask,FOLD_MIN_PIXELS,FOLD_MIN_SIZE)
     if return_plots :
