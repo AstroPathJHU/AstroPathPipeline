067e224b5112bec2929f713354004e09993da842
Maggie Eminizer <margaret.eminizer@gmail.com>
2021-05-17 14:20:18 -0400

    edit run_many_slide_mean_images to run the rest of the PZ slides

--- astropath/scripts/run_many_slide_mean_images.py
+++ astropath/scripts/run_many_slide_mean_images.py
@@ -12,12 +12,42 @@ ET_OFFSET_FILE = r'//bki08/maggie/best_exposure_time_offsets_Polaris_9_8_2020.cs
 #ROOT_DIR = '\\\\bki04/Clinical_Specimen_2'
 #RAWFILE_TOP_DIR = r'//bki07/dat_7'
 #ROOT_DIR = r'//bki04/Clinical_Specimen_7'
-RAWFILE_TOP_DIR = r'//bki07/JHU_Polaris_1'
-ROOT_DIR = r'//bki02/G/VectraPolaris_Data/WholeSlideScans/JHU_Polaris_Melanoma/JHUPolaris_1'
+#RAWFILE_TOP_DIR = r'//bki07/dat_7'
+ROOT_DIR = r'//bki03/Clinical_Specimen_4'
+RAWFILE_TOP_DIR = r'//bki07/dat_4'
+#ROOT_DIR = r'//bki02/G/VectraPolaris_Data/WholeSlideScans/JHU_Polaris_Melanoma/JHUPolaris_1'
 CMD_BASE = f'run_flatfield slide_mean_image --exposure_time_offset_file {ET_OFFSET_FILE} --rawfile_top_dir {RAWFILE_TOP_DIR}'
 CMD_BASE+= f' --root_dir {ROOT_DIR} --n_threads {int(20/N_PROCS)}'
 SLIDE_IDS = [
-'CM1_MP1_Mutliplex_JHUPolaris_1'
+'PZ1',
+'PZ2',
+'PZ3',
+'PZ4',
+'PZ5',
+'PZ6',
+'PZ7',
+'PZ8',
+'PZ9',
+'PZ10',
+#'PZ11', ran this one as a test
+'PZ12',
+'PZ13',
+'PZ14',
+'PZ15',
+'PZ16',
+'PZ17',
+'PZ18',
+'PZ19',
+'PZ20',
+'PZ21',
+'PZ22',
+'PZ23',
+'PZ24',
+'PZ25',
+'PZ26',
+'PZ27',
+'PZ28',
+'PZ29',
 ]
 
 #helper function to run one command with subprocess
