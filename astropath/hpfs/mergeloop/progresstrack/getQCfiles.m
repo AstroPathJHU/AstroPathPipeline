db8dcda24ad104be379cdc03043340fb5813689c
Ben Green <46007058+beng1290@users.noreply.github.com>
2021-05-20 12:11:56 -0700

    Add files via upload

--- astropath/hpfs/mergeloop/progresstrack/getQCfiles.m
+++ astropath/hpfs/mergeloop/progresstrack/getQCfiles.m
@@ -9,85 +9,53 @@
 %% --------------------------------------------------------------------
 %%
 function [QCImagesdate, QCImages]  = getQCfiles(sname, informpath,...
-    Rfd, dRfd, MergeConfig, logstring)    
+    Rfd, MergeConfig, logstring)
+QCImagesdate = [];
+QCImages = [];
+%
+% get the qc files and the tmp files
+%
+QCfd = [informpath,'\Phenotyped\Results\QA_QC'];
+QCfls= dir([QCfd,'\Phenotype\All_Markers\*composite_image.tif']);
+QCtmp = dir([informpath,'\Phenotyped\Results\tmp_ForFiguresTables\*.mat']);
+Rfls = dir([Rfd,'\*table.csv']);
+%
+dRfd = return_date_max(Rfls);
+dQCfls = return_date_max(QCfls);
+dQCtmp = return_date_max(QCtmp);
+%
+% qc folder does not exist and tmp files are not empty create
+%
+for i1 = 1:3
+    %
+    if (~exist(QCfd,'dir') && ~isempty(dQCtmp)) || ...
+            (isempty(QCfls) && ~isempty(dQCtmp)) || ...
+            (~isempty(QCfls) && ~isempty(dQCtmp) ...
+            && datetime(dQCfls) < datetime(dQCtmp))
+        CreateImageQAQC(informpath, sname, MergeConfig, logstring);
+    elseif (exist(QCfd,'dir') && isempty(dRfd)) || ...
+            (exist(QCfd,'dir') && datetime(dRfd) > datetime(dQCfls))
+        rmdir(QCfd,'s')
+    end
+    %
+    % for the loop to triple verify imgaes
+    %
+    QCfls= dir([QCfd,'\Phenotype\All_Markers\*composite_image.tif']);
+    QCtmp = dir([informpath,'\Phenotyped\Results\tmp_ForFiguresTables\*.mat']);
+    Rfls = dir([Rfd,'\*table.csv']);
+    %
+    dRfd = return_date_max(Rfls);
+    dQCfls = return_date_max(QCfls);
+    dQCtmp = return_date_max(QCtmp);
+    %
+end
+%
+if ~isempty(dQCfls)
+    QCImagesdate = dQCfls(1:11);
+    QCImages = length(QCfls);
+else
     QCImagesdate = [];
-    QCImages = [];
-    mergeroot = informpath;
-    %
-    % if QC folder does not exist but the Results folder does then run
-    % Image function
-    %
-    QCfd = [informpath,'\Phenotyped\Results\QA_QC'];
-    QCfl = dir([informpath,'\Phenotyped\Results\tmp_ForFiguresTables\*.mat']);
-    %
-    if exist(Rfd,'dir')
-        Rfd = dir([Rfd,'\*table.csv']);
-    else
-        Rfd = [];
-    end
-    %
-    if ~isempty(Rfd) && ~exist(QCfd,'dir') && ~isempty(QCfl)
-        %
-        % call image loop
-        %
-        CreateImageQAQC(mergeroot, sname, MergeConfig, logstring);
-        %
-    elseif ~isempty(Rfd) && ~exist(QCfd,'dir') && isempty(QCfl)
-         QCImages = 0;
-    end
-    if exist(QCfd,'dir')
-        %
-        % create QC path and get images 
-        %
-        QCfd = [QCfd,'\Phenotype\All_Markers\*composite_image.tif'];
-        QCfiles = dir(QCfd);
-        %
-        QCfl = dir([informpath,'\Phenotyped\Results\tmp_ForFiguresTables']);
-        QCfl = QCfl(3:end);
-        %
-        % if QC output is empty and there are tmp_ForFigureTables then try
-        % to make QC output again
-        %
-        if isempty(QCfiles) && ~isempty(QCfl)
-            CreateImageQAQC(mergeroot, sname, MergeConfig, logstring);
-            %
-            QCfiles = dir(QCfd);
-        end
-        %
-        if ~isempty(QCfiles) 
-            %
-            % get date of most recent file
-            %
-            [~,idx] = max([QCfiles(:).datenum]);
-            dQCfd = QCfiles(idx).date;
-            %
-            % check if Results are newer than QC folder, if they are then
-            % create a new QC output
-            %
-            QCfl = dir([informpath,'\Phenotyped\Results\tmp_ForFiguresTables']);
-            QCfl = QCfl(3:end);
-            %
-            if datetime(dRfd) > datetime(dQCfd)
-                if ~isempty(QCfl)
-                    CreateImageQAQC(mergeroot, sname, MergeConfig, logstring);
-                else 
-                    rmdir(extractBefore(QCfd,'\QA_QC\'),'s');
-                end
-                %
-                QCfiles = dir(QCfd);
-                %
-                % get a new date of most recent file created
-                %
-                [~,idx] = max([QCfiles(:).datenum]);
-            end
-            %
-            % get number of files that QC was generated on
-            %
-            if ~isempty(QCfiles)
-                QCImagesdate = QCfiles(idx).date(1:11);
-                QCImages = length(QCfiles);
-            end
-        end
-        %
-    end
+    QCImages = 0;
+end
+%
 end
