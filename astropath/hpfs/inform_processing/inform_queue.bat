0b0c54a6f2cb061fa1f3825669b3da6d7036504c
Benjamin Green <bgreen42@jh.edu>
2021-06-09 20:36:49 -0400

    move the inform_worker shortcut

---
+++ astropath/hpfs/inform_processing/inform_queue.bat
@@ -0,0 +1,25 @@
+@ECHO OFF
+
+
+SETLOCAL
+
+
+::
+
+
+SET main=\\bki04\astropath_processing\across_project_queues
+SET code=%~dp0\
+
+::
+
+
+sleep 5
+
+
+START PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '%code%inform_queue.ps1' '%main%'"
+
+
+::
+
+
+ENDLOCAL
