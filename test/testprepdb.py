7973b69eb890f39f71c19ec7ee3d1a50bfd599f2
Heshy Roskes <heshyr@gmail.com>
2021-08-02 06:33:01 -0400

    remove testprepdb output

--- test/testprepdb.py
+++ test/testprepdb.py
@@ -1,17 +1,33 @@
-import more_itertools, numpy as np, os, pathlib, PIL.Image, unittest
+import more_itertools, numpy as np, os, pathlib, PIL.Image
 from astropath.shared.csvclasses import Annotation, Batch, Constant, ExposureTime, QPTiffCsv, Region, ROIGlobals, Vertex
 from astropath.shared.overlap import Overlap
 from astropath.shared.rectangle import Rectangle
 from astropath.slides.prepdb.prepdbcohort import PrepDbCohort
 from astropath.slides.prepdb.prepdbsample import PrepDbSample
 from astropath.utilities.misc import checkwindowsnewlines
-from .testbase import assertAlmostEqual, temporarilyreplace
+from .testbase import assertAlmostEqual, temporarilyreplace, TestBaseSaveOutput
 
 thisfolder = pathlib.Path(__file__).parent
 
-class TestPrepDb(unittest.TestCase):
+class TestPrepDb(TestBaseSaveOutput):
   def setUp(self):
     self.maxDiff = None
+
+  @property
+  def outputfilenames(self):
+    SlideIDs = "M21_1", "YZ71", "M206"
+    return [
+      thisfolder/"test_for_jenkins"/"prepdb"/SlideID/"dbload"/filename.name
+      for SlideID in SlideIDs
+      for ext in ("csv", "jpg")
+      for filename in (thisfolder/"data"/"reference"/"alignment"/SlideID/"dbload").glob(f"*.{ext}")
+    ] + [
+      thisfolder/"test_for_jenkins"/"alignment"/SlideID/"logfiles"/f"{SlideID}-align.log"
+      for SlideID in SlideIDs
+    ] + [
+      thisfolder/"test_for_jenkins"/"alignment"/"logfiles"/"align.log",
+    ]
+
 
   def testPrepDb(self, SlideID="M21_1", units="safe", skipannotations=False):
     dbloadroot = thisfolder/"test_for_jenkins"/"prepdb"
@@ -29,39 +45,46 @@ class TestPrepDb(unittest.TestCase):
     args = [os.fspath(thisfolder/"data"), "--sampleregex", SlideID, "--debug", "--units", units, "--xmlfolder", os.fspath(thisfolder/"data"/"raw"/SlideID), "--allow-local-edits", "--ignore-dependencies", "--rerun-finished", "--rename-annotation", "Good tisue", "Good tissue", "--dbloadroot", os.fspath(dbloadroot), "--logroot", os.fspath(dbloadroot)]
     if skipannotations:
       args.append("--skip-annotations")
-    PrepDbCohort.runfromargumentparser(args)
-    sample = PrepDbSample(thisfolder/"data", SlideID, uselogfiles=False, xmlfolders=[thisfolder/"data"/"raw"/SlideID], dbloadroot=dbloadroot, logroot=dbloadroot)
 
-    for filename, cls, extrakwargs in (
-      (f"{SlideID}_annotations.csv", Annotation, {}),
-      (f"{SlideID}_batch.csv", Batch, {}),
-      (f"{SlideID}_constants.csv", Constant, {}),
-      (f"{SlideID}_exposures.csv", ExposureTime, {}),
-      (f"{SlideID}_globals.csv", ROIGlobals, {}),
-      (f"{SlideID}_overlap.csv", Overlap, {"nclip": sample.nclip, "rectangles": sample.rectangles}),
-      (f"{SlideID}_qptiff.csv", QPTiffCsv, {}),
-      (f"{SlideID}_rect.csv", Rectangle, {}),
-      (f"{SlideID}_vertices.csv", Vertex, {}),
-      (f"{SlideID}_regions.csv", Region, {}),
-    ):
-      if filename == "M21_1_globals.csv": continue
-      if skipannotations and cls in (Annotation, Vertex, Region):
-        self.assertFalse((dbloadroot/SlideID/"dbload"/filename).exists())
-        continue
-      try:
-        rows = sample.readtable(dbloadroot/SlideID/"dbload"/filename, cls, checkorder=True, checknewlines=True, extrakwargs=extrakwargs)
-        targetrows = sample.readtable(thisfolder/"data"/"reference"/"prepdb"/SlideID/filename, cls, checkorder=True, checknewlines=True, extrakwargs=extrakwargs)
-        for i, (row, target) in enumerate(more_itertools.zip_equal(rows, targetrows)):
-          assertAlmostEqual(row, target, rtol=1e-5, atol=8e-7)
-      except:
-        raise ValueError("Error in "+filename)
+    try:
+      PrepDbCohort.runfromargumentparser(args)
+      sample = PrepDbSample(thisfolder/"data", SlideID, uselogfiles=False, xmlfolders=[thisfolder/"data"/"raw"/SlideID], dbloadroot=dbloadroot, logroot=dbloadroot)
 
-    with PIL.Image.open(dbloadroot/SlideID/"dbload"/f"{SlideID}_qptiff.jpg") as img, \
-         PIL.Image.open(thisfolder/"data"/"reference"/"prepdb"/SlideID/f"{SlideID}_qptiff.jpg") as targetimg:
-      np.testing.assert_array_equal(np.asarray(img), np.asarray(targetimg))
+      for filename, cls, extrakwargs in (
+        (f"{SlideID}_annotations.csv", Annotation, {}),
+        (f"{SlideID}_batch.csv", Batch, {}),
+        (f"{SlideID}_constants.csv", Constant, {}),
+        (f"{SlideID}_exposures.csv", ExposureTime, {}),
+        (f"{SlideID}_globals.csv", ROIGlobals, {}),
+        (f"{SlideID}_overlap.csv", Overlap, {"nclip": sample.nclip, "rectangles": sample.rectangles}),
+        (f"{SlideID}_qptiff.csv", QPTiffCsv, {}),
+        (f"{SlideID}_rect.csv", Rectangle, {}),
+        (f"{SlideID}_vertices.csv", Vertex, {}),
+        (f"{SlideID}_regions.csv", Region, {}),
+      ):
+        if filename == "M21_1_globals.csv": continue
+        if skipannotations and cls in (Annotation, Vertex, Region):
+          self.assertFalse((dbloadroot/SlideID/"dbload"/filename).exists())
+          continue
+        try:
+          rows = sample.readtable(dbloadroot/SlideID/"dbload"/filename, cls, checkorder=True, checknewlines=True, extrakwargs=extrakwargs)
+          targetrows = sample.readtable(thisfolder/"data"/"reference"/"prepdb"/SlideID/filename, cls, checkorder=True, checknewlines=True, extrakwargs=extrakwargs)
+          for i, (row, target) in enumerate(more_itertools.zip_equal(rows, targetrows)):
+            assertAlmostEqual(row, target, rtol=1e-5, atol=8e-7)
+        except:
+          raise ValueError("Error in "+filename)
 
-    for log in logs:
-      checkwindowsnewlines(log)
+      with PIL.Image.open(dbloadroot/SlideID/"dbload"/f"{SlideID}_qptiff.jpg") as img, \
+           PIL.Image.open(thisfolder/"data"/"reference"/"prepdb"/SlideID/f"{SlideID}_qptiff.jpg") as targetimg:
+        np.testing.assert_array_equal(np.asarray(img), np.asarray(targetimg))
+
+      for log in logs:
+        checkwindowsnewlines(log)
+    except:
+      self.saveoutput()
+      raise
+    finally:
+      self.removeoutput()
 
   def testPrepDbFastUnits(self, SlideID="M21_1"):
     self.testPrepDb(SlideID, units="fast")
