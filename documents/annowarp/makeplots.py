0a62025c15dc8ef9ac1be25a731d54af5ad4a0b3
Heshy Roskes <heshyr@gmail.com>
2021-10-26 10:05:17 -0400

    fix annowarp plots

--- documents/annowarp/makeplots.py
+++ documents/annowarp/makeplots.py
@@ -9,15 +9,18 @@ data = here/".."/".."/"test"/"data"
 zoomroot = here/".."/".."/"test"/"data"/"reference"/"zoom"
 annowarproot = here/".."/".."/"test"/"data"/"reference"/"annowarp"
 alignroot = here/".."/".."/"test"/"data"/"reference"/"alignment"/"component_tiff"
+prepdbroot = here/".."/".."/"test"/"data"/"reference"/"prepdb"
 samp = "M206"
 
 def makeplots():
   with tempfile.TemporaryDirectory() as dbloadroot:
     dbloadroot = pathlib.Path(dbloadroot)
     (dbloadroot/samp/"dbload").mkdir(parents=True)
-    for root in annowarproot, alignroot:
-      for filename in (root/samp/"dbload").glob("*.csv"):
-        shutil.copy(filename, dbloadroot/samp/"dbload")
+    for root in prepdbroot, annowarproot, alignroot:
+      for folder in root/samp, root/samp/"dbload":
+        for filename in folder.glob("*.csv"):
+          shutil.copy(filename, dbloadroot/samp/"dbload")
+          print(filename)
     from ...test.testzoom import gunzipreference
     gunzipreference(samp)
     A = AnnoWarpSampleInformTissueMask(data, samp, zoomroot=zoomroot, dbloadroot=dbloadroot, annotationsynonyms={"Good tisue": "Good tissue"})
@@ -27,23 +30,22 @@ def makeplots():
 
     with A.using_images() as (wsi, fqptiff):
       zoomlevel = fqptiff.zoomlevels[0]
-      apscale = zoomlevel.qpscale
       qptiff = PIL.Image.fromarray(zoomlevel[A.qptifflayer-1].asarray())
       wsi = np.asarray(wsi)
 
-    oneqppixel = units.onepixel(apscale)
+    oneqppixel = units.onepixel(A.apscale)
     ylim = np.array((9500, 10500)) * oneqppixel
     xlim = np.array((7000, 7500)) * oneqppixel
     if tuple(xlim):
-      xlimpscale = units.convertpscale(xlim, apscale, A.pscale)
+      xlimpscale = units.convertpscale(xlim, A.apscale, A.pscale)
     else:
       xlimpscale = xlim
     if tuple(ylim):
-      ylimpscale = units.convertpscale(ylim, apscale, A.pscale)
+      ylimpscale = units.convertpscale(ylim, A.apscale, A.pscale)
     else:
       ylimpscale = ylim
 
-    showannotation(qptiff, A.regions, vertices=A.apvertices, imagescale=apscale, figurekwargs={}, ylim=ylim, xlim=xlim, saveas=here/"qptiff.pdf")
+    showannotation(qptiff, A.regions, vertices=A.apvertices, imagescale=A.apscale, figurekwargs={}, ylim=ylim, xlim=xlim, saveas=here/"qptiff.pdf")
     showannotation(wsi, warpedregions, imagescale=A.pscale, figurekwargs={}, ylim=ylimpscale, xlim=xlimpscale, saveas=here/"wsi.pdf")
 
 if __name__ == "__main__":
