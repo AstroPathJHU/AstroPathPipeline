80875ccd17977301469f4ade2e1bf19723c10c77
Heshy Roskes <heshyr@gmail.com>
2021-10-29 09:01:57 -0400

    fix tests

--- test/teststitchmask.py
+++ test/teststitchmask.py
@@ -13,6 +13,12 @@ class TestStitchMask(TestBaseCopyInput, TestBaseSaveOutput):
       newfolder = thisfolder/"test_for_jenkins"/"stitchmask"/SlideID/"im3"/"meanimage"/"image_masking"
       for file in oldfolder.glob("*tissue_mask.bin"):
         yield file, newfolder
+
+      newfolder = thisfolder/"test_for_jenkins"/"stitchmask"/SlideID/"dbload"
+      for oldfolder in thisfolder/"data"/SlideID/"dbload", thisfolder/"data"/"reference"/"alignment"/SlideID/"dbload":
+        for file in oldfolder.glob("*.csv"):
+          yield file, newfolder
+
   @property
   def outputfilenames(self):
     return [
@@ -38,14 +44,14 @@ class TestStitchMask(TestBaseCopyInput, TestBaseSaveOutput):
     }[masktype]
 
     dbloadroot = None
-    if SlideID == "M21_1": dbloadroot = thisfolder/"data"/"reference"/"alignment"
+    if SlideID == "M21_1": dbloadroot = maskroot
 
     args = [os.fspath(root), "--maskroot", os.fspath(maskroot), "--logroot", os.fspath(maskroot), "--mask-file-suffix", maskfilesuffix, "--allow-local-edits", "--sampleregex", SlideID+"$", "--debug", "--ignore-dependencies", "--rerun-finished", "--units", units]
     if dbloadroot is not None: args += ["--dbloadroot", os.fspath(dbloadroot)]
     cohortcls.runfromargumentparser(args)
 
     sample = samplecls(root, SlideID, maskroot=maskroot, logroot=maskroot, dbloadroot=dbloadroot, maskfilesuffix=maskfilesuffix)
-    refsample = samplecls(root, SlideID, maskroot=thisfolder/"data"/"reference"/"stitchmask", logroot=maskroot, dbloadroot=dbloadroot)
+    refsample = samplecls(root, SlideID, maskroot=thisfolder/"data"/"reference"/"stitchmask", logroot=maskroot, dbloadroot=dbloadroot, maskfilesuffix=maskfilesuffix)
 
     try:
       mask1 = sample.readmask()
