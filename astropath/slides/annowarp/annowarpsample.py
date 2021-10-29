eceefde354239b65497fb37b6c5c47eea906fcbf
Heshy Roskes <heshyr@gmail.com>
2021-10-29 09:02:47 -0400

    bite the bullet on floattoint

--- astropath/slides/annowarp/annowarpsample.py
+++ astropath/slides/annowarp/annowarpsample.py
@@ -259,8 +259,8 @@ class AnnoWarpSampleBase(QPTiffSample, WSISample, WorkflowSample, XMLPolygonRead
     qptiffzoom = np.asarray(qptiffzoom.resize(np.array(qptiffzoom.size)//zoomfactor))
     firstresult = computeshift((qptiffzoom, wsizoom), usemaxmovementcut=False)
 
-    initialdx = floattoint(float(np.rint(firstresult.dx.n * zoomfactor / (self.tilesize/self.oneimpixel)) * (self.tilesize/self.oneimpixel)), rtol=1e-4)
-    initialdy = floattoint(float(np.rint(firstresult.dy.n * zoomfactor / (self.tilesize/self.oneimpixel)) * (self.tilesize/self.oneimpixel)), rtol=1e-4)
+    initialdx = floattoint(float(np.rint(firstresult.dx.n * zoomfactor / (self.tilesize/self.oneimpixel)) * np.rint(self.tilesize/self.oneimpixel)), rtol=1e-4)
+    initialdy = floattoint(float(np.rint(firstresult.dy.n * zoomfactor / (self.tilesize/self.oneimpixel)) * np.rint(self.tilesize/self.oneimpixel)), rtol=1e-4)
 
     if initialdx or initialdy:
       self.logger.warningglobal(f"found a relative shift of {firstresult.dx*zoomfactor, firstresult.dy*zoomfactor} pixels between the qptiff and wsi")
