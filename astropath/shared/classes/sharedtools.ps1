38210ec893773881b0feacef85b90d0407b42a83
beng1290 <beng1290@gmail.com>
2021-11-01 17:39:28 -0400

    updating shared tools so that it is more generalizable

--- astropath/shared/classes/sharedtools.ps1
+++ astropath/shared/classes/sharedtools.ps1
@@ -16,65 +16,217 @@
     [string]$pyinstalllocation
     [string]$pyenv 
     [string]$pyinstalllog
-    <# -----------------------------------------
-     OpenCSVFile
-     open a csv file with error checking into a
-     pscustom object
-     ------------------------------------------
-     Input: 
-        -fpath[string]: file path to read in
-     ------------------------------------------
-     Usage: OpenCSVFile(fpath)
-    ----------------------------------------- #>
+    [string]$package = 'astropath'
+    [string]$pypackagepath
+    #
     sharedtools(){
         $this.createpypaths()
     }
-    #
+    <# -----------------------------------------
+     createpypaths
+     initializes the python paths and activates
+     conda commands 
+     ------------------------------------------
+     Usage: $this.createpypaths()
+    ----------------------------------------- #>
     [void]createpypaths(){
         #
-        $this.pyinstalllocation = '\\'+$this.defserver()+'\c$\users\public\astropath\py\'
-        $this.pyenv = $this.pyinstalllocation + 'astropathworkflow'
+        $this.checkconda()
+        $this.pyinstalllocation = '\\'+$this.defserver()+'\c$\users\public\'+$this.package +'\py\'
+        $this.pyenv = $this.pyinstalllocation + $this.package+'workflow'
         $this.pyinstalllog = $this.pyinstalllocation + 'pyinstall.log'
+        $this.defCodeRoot()
+        $this.pypackagepath = $this.coderoot + '\..\.'
         #
     } 
-    #
+    <# -----------------------------------------
+     defCodeRoot
+     the path to the powershell module (package)
+     ------------------------------------------
+     Usage: $this.defCodeRoot()
+    ----------------------------------------- #>
     [void]defCodeRoot(){
         #
         $root = $this.defRoot()
         #
-        $folder = $root -Split('\\astropath\\')
-        $this.coderoot = $folder[0] + '\astropath'
+        $folder = $root -Split('\\'+$this.package+'\\')
+        $this.coderoot = $folder[0] + '\'+$this.package
     }
-    #
+    <# -----------------------------------------
+     GetVersion
+     get the version number and check that the 
+     module is supported by that version. If 
+     the version number was added as >v0.0.1
+     then the version number should be selected
+     based on the full git development tag. If
+     the version number was added as v0.0.1
+     just return v0.0.1.
+     ------------------------------------------
+     Usage: $this.GetVersion()
+    ----------------------------------------- #>
     [string]GetVersion($mpath, $module, $project){
         #
         $configfile = $this.ImportConfigInfo($mpath)
         $vers = ($configfile | Where-Object {$_.Project -eq $project}).($module+'version')
         if (!$vers){
             Throw 'No version number found'
-        } elseif ($vers -ne '0.0.1'){
-            if ($module -contains  @('batchflatfield')){
-                Throw 'batchflatfield is run from the meanimagecomparison module ' +
-                    'and is not initiated in powershell for version: ' + $vers    
-            }
-            #
-            try {
-                $l = $this.getpythonvers()
-            } catch{
-                $this.checkconda()
-                $this.checkpyapenvir()
-                $l = $this.getpythonvers()
-            }
-            #
-            if ($vers -match $l){
-                $vers = $l
-            }
-            #
-        } elseif ($module -contains  @('meanimagecomparison', 'warping')){
-            Throw 'module not supported in this version (' + $vers + '): ' + $module
         }
+        #
+        if ($this.apversionchecks($mpath, $module, $vers)){
+            return $vers
+        }
+        # 
+        $vers = $this.getfullversion()
         return $vers
         #
+    }
+    <# -----------------------------------------
+     APVersionChecks
+     version checking applied specifically for the 
+     astropath pipeline. This is the only method
+     specific to the astropath package.
+     ------------------------------------------
+     Usage: $this.APVersionChecks()
+    ----------------------------------------- #>
+    [switch]APVersionChecks($mpath, $module, $vers){
+        #
+        if ($vers -eq 'v0.0.1' -and ($module -contains  @('meanimagecomparison', 'warping'))){
+            Throw 'module not supported in this version (' + $vers + '): ' + $module
+        } elseif ($vers -ne 'v0.0.1' -and $module -contains  @('batchflatfield')) {
+             Throw 'batchflatfield is run from the meanimagecomparison module ' +
+                    'and is not initiated in powershell for version: ' + $vers    
+        }
+        #
+        if ($this.package -match 'astropath' -and $vers -eq '0.0.1'){
+            return $true
+        } else {
+            return $false
+        }
+        #
+    }
+    <# -----------------------------------------
+    getfullversion
+    get the full version number for this branch.
+    if the path is a git repository pull the 
+    github version and check if the git branch
+    is clean. If it isn't add the date. If the
+    path is not a git repo try to get the version
+    number from the conda envrionment, if the
+    package \ envir do not exist return the 
+    v0.0.0... version number.
+    This logic is from:
+    https://pypi.org/project/setuptools-scm/ 
+    ------------------------------------------
+     Usage: $this.getfullversion()
+    ----------------------------------------- #>
+    [string]getfullversion(){
+        #
+        if ($this.checkgitrepo()){
+            $version = $this.getgitversion()
+            if (!($this.checkgitstatus())){
+                $version = $version, $this.getdate() -join '.'
+            } 
+        } else {
+            $version = $this.getpackageversion() 
+            $version = $version, $this.getdate() -join '.' 
+        }
+        #
+        return $version
+    }
+    <# -----------------------------------------
+    getdate
+    ------------------------------------------
+     Usage: $this.getdate()
+     get the date in the version formatting
+    ----------------------------------------- #>
+    [string]getdate(){
+        return ("d"+(Get-Date -format "yyyyMMdd"))
+    }
+    <# -----------------------------------------
+    checkgitinstalled
+    ------------------------------------------
+     Usage: $this.checkgitinstalled()
+     if git is a command then returns true 
+     else returns false
+    ----------------------------------------- #>
+    [switch]checkgitinstalled(){
+        try {
+            $gitversion = git --version
+            return $true
+        } catch {
+            return $false
+        }
+    }
+    <# -----------------------------------------
+    checkgitrepo
+    if git is installed check if the tree is a
+    git repo and return true. If git is not 
+    installed or it is not a git repo will return
+    false
+    ------------------------------------------
+     Usage: $this.checkgitrepo()
+    ----------------------------------------- #>
+    [switch]checkgitrepo(){
+        if ($this.checkgitinstalled()){
+            try {
+               $gitrepo = git -C $this.pypackagepath rev-parse --is-inside-work-tree
+               return $true
+            } catch {
+               return $false
+            }
+        } else {
+            return $false
+        }
+    }
+    <# -----------------------------------------
+    checkgitstatus
+    ------------------------------------------
+     Usage: $this.checkgitstatus()
+     check the git status, if the working tree is
+     clean return true else return false
+    ----------------------------------------- #>
+    [switch]checkgitstatus(){
+           $gitstatus = git -C $this.pypackagepath status
+           if ($gitstatus -match "nothing to commit, working tree clean"){
+                return $true
+           } else {
+                return $false
+           }
+    }
+    <# -----------------------------------------
+    getgitversion
+    ------------------------------------------
+     Usage: $this.checkgitstatus()
+     get the git version in the astropath format
+    ----------------------------------------- #>
+    [string]getgitversion(){
+        $v = git -C $this.pypackagepath describe --tags --long
+        $v2 = $v -split '-'
+        $v3 = $v2[0] -split '\.'
+        $v4 = [int]$v3[2] + 1
+        $v5 = $v3[0], $v3[1], $v4, ('dev'+$v2[1] + '+' + $v2[2]) -join '.'
+        return $v5
+    }
+    <# -----------------------------------------
+    getpackageversion
+    ------------------------------------------
+     Usage: $this.getpackageversion()
+     get the package version in the astropath 
+     format from the enirvonment install. if the
+     package or environment is not installed 
+     for some reason return the v0.0.0 versioning
+    ----------------------------------------- #>
+    [string]getpackageversion(){
+        $version = "v0.0.0.dev0+g0000000"
+        if ($this.CheckpyEnvir()){
+            #
+            $condalist = conda list -p $this.pyenv
+            $astropath = $condalist -match $this.package
+            if ($astropath[1]){
+                $version = 'v'+($astropath[1] -split ' ') -match 'dev'
+            }
+        }
+        return $version                
     }
     <# -----------------------------------------
      checkconda
@@ -116,49 +268,33 @@
              }
         }
      <# -----------------------------------------
-     astropathpypath
-     return the path to the astropath python 
-     package root location.
-     ------------------------------------------
-     Usage: $this.astropathpypath()
-    ----------------------------------------- #>    
-    [string]AstroPathpyPath(){
-        $this.defCodeRoot()
-        $astropathpypath = $this.coderoot + '\..\.'
-        return($astropathpypath)
-    }
-     <# -----------------------------------------
-     CheckpyAPEnvir
-     Check if py\astropathworkflow conda environment
+     CheckpyEnvir
+     Check if py\<packagename>workflow conda environment
      exists. If it does not create it and install
      the astropath package from the current working.
      If it does exist check for updates.
      ------------------------------------------
-     Usage: $this.CheckpyAPEnvir()
+     Usage: $this.CheckpyEnvir()
     ----------------------------------------- #>        
-    [void]CheckpyAPEnvir(){
+    [switch]CheckpyEnvir(){
         #
         try {
             conda activate $this.pyenv 2>&1 >> $this.pyinstalllog
             conda deactivate $this.pyenv 2>&1 >> $this.pyinstalllog
+            return $true
         } catch {
-            $this.createpyapenvir()
+            return $false
         }
         #
     }
     #
-    [void]CheckpyAPEnvir($u){
-        #
-        $this.upgradepyapenvir()
-        #
-    }
     <# -----------------------------------------
-     CreatepyAPEnvir
-     create py\astropathworkflow conda environment.
+     CreatepyEnvir
+     create py\<packagename>workflow conda environment.
      ------------------------------------------
-     Usage: $this.createpyapenvir()
+     Usage: $this.createpyenvir()
     ----------------------------------------- #>      
-    [void]CreatepyAPEnvir(){
+    [void]CreatepyEnvir(){
         $this.createdirs($this.pyinstalllocation)
         conda create -y -p $this.pyenv python=3.8 2>&1 >> $this.pyinstalllog
         $this.PopFile($this.pyinstalllog, ($this.pyenv + " CONDA ENVIR CREATED"))
@@ -167,35 +303,23 @@
         conda install -y -c conda-forge pyopencl gdal cvxpy numba 'ecos!=2.0.8' git `
               2>&1 >> $this.pyinstalllog
         $this.PopFile($this.pyinstalllog, ($this.pyenv + " CONDA ENVIR INSTALLS COMPLETE"))
-        pip -q install $this.astropathpypath() 2>&1 >> $this.pyinstalllog
+        pip -q install $this.pypackagepath 2>&1 >> $this.pyinstalllog
         $this.PopFile($this.pyinstalllog, ($this.pyenv + " PIP INSTALLS COMPLETE"))
         conda deactivate $this.pyenv 2>&1 >> $this.pyinstalllog
     }
     <# -----------------------------------------
-     upgradepyapenvir
-     create py\astropathworkflow conda environment.
+     upgradepyenvir
+     create py\<packagename>workflow conda environment.
      ------------------------------------------
-     Usage: $this.upgradepyapenvir()
+     Usage: $this.upgradepyenvir()
     ----------------------------------------- #>    
-    [void]UpgradepyAPEnvir(){
+    [void]UpgradepyEnvir(){
         try{
             conda activate $this.pyenv 2>&1 >> $this.pyinstalllog
-            pip -q install -U $this.astropathpypath()  2>&1 >> $this.pyinstalllog
+            pip -q install -U $this.pypackagepath  2>&1 >> $this.pyinstalllog
             conda deactivate $this.pyenv 2>&1 >> $this.pyinstalllog
         } catch {
-            $this.createpyapenvir()
+            $this.createpyenvir()
         }
-    }
-    <# -----------------------------------------
-     getpythonvers
-     get the version number from python
-     ------------------------------------------
-     Usage: $this.getpythonvers()
-    ----------------------------------------- #>   
-    [string]getpythonvers(){
-        conda activate $this.pyenv 2>&1 >> $this.pyinstalllog
-        $l = python -c "from astropath.utilities.version import astropathversion; print(astropathversion)"
-        conda deactivate $this.pyenv 2>&1 >> $this.pyinstalllog
-        return($l)
     }
 }
