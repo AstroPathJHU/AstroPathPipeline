8d200a584eb3607a10a950ff3646669b27e76274
Heshy Roskes <heshyr@gmail.com>
2021-11-04 09:12:58 -0400

    help message for --print-threshold

--- astropath/shared/argumentparser.py
+++ astropath/shared/argumentparser.py
@@ -167,7 +167,7 @@ class RunFromArgumentParser(ArgumentParserWithVersionRequirement, ThingWithRoots
     g.add_argument("--logroot", type=pathlib.Path, help="root location where the log files are stored (default: same as root).")
     g.add_argument("--no-log", action="store_true", help="do not write to log files.")
     p.add_argument("--skip-start-finish", action="store_true", help="do not write the START: and FINISH: lines to the log (this should only be used if external code writes those lines).")
-    p.add_argument("--print-threshold", choices=("all", "info", "warning", "error", "critical", "none"), default="all")
+    p.add_argument("--print-threshold", choices=("all", "info", "warning", "error", "critical", "none"), default="all", help="minimum level of log messages that should be printed to stderr (default: all)")
     g = p.add_mutually_exclusive_group()
     g.add_argument("--no-dev-version", help="refuse to run unless the package version is tagged.", action="store_const", const="tag", dest="version_requirement")
     g.add_argument("--allow-dev-version", help="ok to run even if the package is at a dev version (default if using a log file).", action="store_const", const="commit", dest="version_requirement")
