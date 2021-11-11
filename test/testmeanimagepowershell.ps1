a306d2faa086d5f0b3f0102c0c7f7ff3c8110651
andrewjorquera <andrewjorq@gmail.com>
2021-11-11 15:13:36 -0500

    change python test to powershell

---
+++ test/testmeanimagepowershell.ps1
@@ -0,0 +1,13 @@
+﻿Import-Module 'H:\andrew\AstroPathPipelinePrivate\astropath'
+$task = ('1', 'M21_1', '\\bki08\h$\testing\testing_meanimage', '\\bki08\h$\testing\astropath_processing')
+$inp = meanimage $task
+
+$inp.DownloadFiles()
+
+$inp.ShredDat()
+
+$inp.GetMeanImage()
+
+$inp.returndata()
+
+$inp.cleanup()
