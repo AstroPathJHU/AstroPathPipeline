3fa91f7ab2de295da5c38c12621f55f9aad0fd9a
andrewjorquera <andrewjorq@gmail.com>
2021-11-19 10:06:07 -0500

    meanimage powershell test draft

--- test/testmeanimagepowershell.ps1
+++ test/testmeanimagepowershell.ps1
@@ -1,13 +1,47 @@
-﻿Import-Module 'H:\andrew\AstroPathPipelinePrivate\astropath'
-$task = ('1', 'M21_1', '\\bki08\h$\testing\testing_meanimage', '\\bki08\h$\testing\astropath_processing')
+﻿#Set up testing environment
+$modulelocation = $PSScriptRoot + '\..\astropath'
+Import-Module $modulelocation
+$processing = $PSScriptRoot + '\test_for_jenkins\testing_meanimage'
+$datalocation = $PSScriptRoot + '\data'
+$task = ('1', 'M21_1', $processing, $datalocation)
 $inp = meanimage $task
 
+#Download Files test
 $inp.DownloadFiles()
+$xmlpath = $inp.processvars[1] + '\' + $inp.sample.slideid + '\*.xml'
+$im3path = $inp.processvars[2] + '\..\Scan1\MSI\*.im3'
+if (!(@(Test-Path $xmlpath) -and @(Test-Path $im3path))) {
+    Write-Error 'Download Files Test Failed'
+}
 
+#Shred Data.dat files test
 $inp.ShredDat()
+$datpath = $inp.processvars[1] + '\' + $inp.sample.slideid + '\*.dat'
+if (!(@(Test-Path $datpath))) {
+    Write-Error 'Shred Dat Test Failed'
+}
 
+#Get Mean Image test
 $inp.GetMeanImage()
+$meanimagepath = $inp.processvars[0] + '\meanimage'
+if (!(@(Test-Path $meanimagedatpath))) {
+    Write-Error 'Get Mean Image Test Failed'
+}
 
+#Return Data test
 $inp.returndata()
+$returnpath = $inp.sample.im3folder() + '\meanimage'
+if (!(@(Test-Path $meanimagedatpath))) {
+    Write-Error 'Return Data Test Failed'
+}
 
+#Cleanup test
 $inp.cleanup()
+if ($inp.processvars[4]) {
+    if (@(Test-Path $inp.processvars[0])) {
+        Write-Error 'Cleanup Test Failed'
+    }
+}
+
+#Remove temporary processing directory
+$inp.sample.removedir($processing)
