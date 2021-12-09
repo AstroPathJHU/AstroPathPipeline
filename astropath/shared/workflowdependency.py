92978827936795df4156190bbcfbab794f63c2b3
Heshy Roskes <heshyr@gmail.com>
2021-12-09 13:24:44 -0500

    convert generator to list!

--- astropath/shared/workflowdependency.py
+++ astropath/shared/workflowdependency.py
@@ -61,7 +61,7 @@ class WorkflowDependency(ThingWithRoots):
     Files that are saved from one run to the next so that we can
     resume where we left off.  These are removed by cleanup()
     """
-    return self.getworkinprogressfiles(**self.workflowkwargs)
+    return list(self.getworkinprogressfiles(**self.workflowkwargs))
 
   def cleanup(self):
     printed = False
@@ -99,8 +99,8 @@ class WorkflowDependency(ThingWithRoots):
       SlideID=SlideID,
       samplelog=cls.getlogfile(**workflowkwargs),
       module=cls.logmodule(),
-      missingfiles=cls.getmissingoutputfiles(**workflowkwargs),
-      workinprogressfiles=cls.getworkinprogressfiles(**workflowkwargs),
+      missingfiles=list(cls.getmissingoutputfiles(**workflowkwargs)),
+      workinprogressfiles=list(cls.getworkinprogressfiles(**workflowkwargs)),
       startregex=cls.logstartregex(),
       endregex=cls.logendregex(),
     )
