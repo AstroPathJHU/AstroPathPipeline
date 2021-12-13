9b87c2796102e162386a62ed0cd60510d02366a7
andrewjorquera <andrewjorq@gmail.com>
2021-12-13 11:58:04 -0500

    add compute checksum for copyutils

--- astropath/shared/classes/copyutils.ps1
+++ astropath/shared/classes/copyutils.ps1
@@ -8,7 +8,7 @@
  -------------------------------------------#>
 class copyutils{
     #
-    copyutiles(){}
+    copyutils(){}
     #
     [void]copy([string]$sor, [string]$des){
         xcopy $sor, $des /q /y /z /j /v | Out-Null
@@ -16,26 +16,34 @@ class copyutils{
     #
     [void]copy([string]$sor, [string]$des, [string]$filespec){
         if ($filespec -match '\*'){
-            robocopy $sor $des -r:3 -w:3 -np -mt:1 | out-null
+            robocopy $sor $des -r:3 -w:3 -np -s -mt:1 | out-null
         } else {
             $filespec = '*' + $filespec
-            robocopy $sor $des $filespec -r:3 -w:3 -np -mt:1 | out-null
+            robocopy $sor $des $filespec -r:3 -w:3 -np -s -mt:1 | out-null
         }
+        $this.verifyChecksum($sor, $des)
     }
     #
     [void]copy([string]$sor, [string]$des, [string]$filespec, [int]$threads){
         if ($filespec -match '\*'){
-            robocopy $sor $des -r:3 -w:3 -np -mt:$threads | out-null
+            robocopy $sor $des -r:3 -w:3 -np -s -mt:$threads | out-null
         } else {
             $filespec = '*' + $filespec
-            robocopy $sor $des $filespec -r:3 -w:3 -np -mt:$threads | out-null
+            robocopy $sor $des $filespec -r:3 -w:3 -np -s -mt:$threads | out-null
         }
-        #
-
+        $this.verifyChecksum($sor, $des)
+    }
+    #
+    [void]verifyChecksum([string]$sor, [string]$des){
         $sourcehash = Get-ChildItem -Path ($sor+'\*') -Recurse | Get-FileHash -Algorithm MD5
         $destinationhash = Get-ChildItem -Path ($des+'\*') -Recurse | Get-FileHash -Algorithm MD5
-
-
+        $comparison = Compare-Object -ReferenceObject $sourcehash -DifferenceObject $destinationhash -Property Hash | Where-Object -FilterScript {$_.SideIndicator -eq '<='}
+        if ($comparison -ne $null) {
+            foreach ($file in $comparison) {
+                $tempsor = ($sourcehash -match $file.Hash).Path
+                xcopy $tempsor, $des /q /y /z /j /v | Out-Null
+            }
+        }
     }
     #
 }
