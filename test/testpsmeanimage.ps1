2b1fdfeca58ffbf1d11613ba7ef84ecbebdd851c
andrewjorquera <andrewjorq@gmail.com>
2022-01-05 14:07:59 -0500

    add debug strings

--- test/testpsmeanimage.ps1
+++ test/testpsmeanimage.ps1
@@ -1,7 +1,7 @@
 ﻿<# -------------------------------------------
  testpsmeanimage
  created by: Andrew Jorquera
- Last Edit: 12/17/2021
+ Last Edit: 1/5/2021
  --------------------------------------------
  Description
  test if the methods of meanimage are 
@@ -33,34 +33,42 @@ Class testpsmeanimage {
     }
     #
     [void]DownloadFilesTest(){
+        Write-Host 'Starting Download Files Test'
         $this.DownloadFiles()
-        $xmlpath = $this.processvars[1] + '\' + $this.sample.slideid + '\*.xml'
-        $im3path = $this.processvars[2] + '\..\Scan1\MSI\*.im3'
+        $xmlpath = $this.processvars[1] + '/' + $this.sample.slideid + '/*.xml'
+        Write-Host 'xml path: ' $xmlpath
+        $im3path = $this.processvars[2] + '/../Scan1/MSI/*.im3'
         if (!(@(Test-Path $xmlpath) -and @(Test-Path $im3path))) {
             Write-Error 'Download Files Test Failed'
             exit 1
         }
+        Write-Host 'Passed Download Files Test'
     }
     #
     [void]ShredDatTest(){
+        Write-Host 'Starting Shred Dat Test'
         $this.ShredDat()
-        $datpath = $this.processvars[1] + '\' + $this.sample.slideid + '\*.dat'
+        $datpath = $this.processvars[1] + '/' + $this.sample.slideid + '/*.dat'
         if (!(@(Test-Path $datpath))) {
             Write-Error 'Shred Dat Test Failed'
             exit 1
         }
+        Write-Host 'Passed Shred Dat Test'
     }
     #
     [void]ReturnDataTest(){
+        Write-Host 'Starting Return Data Test'
         $this.returndata()
         $returnpath = $this.sample.im3folder() + '\meanimage'
         if (!(@(Test-Path $returnpath))) {
             Write-Error 'Return Data Test Failed'
             exit 1
         }
+        Write-Host 'Passed Return Data Test'
     }
     #
     [void]CleanupTest(){
+        Write-Host 'Starting Cleanup Test'
         $this.cleanup()
         if ($this.processvars[4]) {
             if (@(Test-Path $this.processvars[0])) {
@@ -68,6 +76,7 @@ Class testpsmeanimage {
                 exit 1
             }
         }
+        Write-Host 'Passed Cleanup Test'
     }
 }
 #
