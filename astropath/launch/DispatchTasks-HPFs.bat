b384f89c153eb25aa488476fa5b1f0227e6a59f8
beng1290 <beng1290@gmail.com>
2022-01-12 14:17:55 -0500

    something weird in the batch files

--- astropath/launch/DispatchTasks-HPFs.bat
+++ astropath/launch/DispatchTasks-HPFs.bat
@@ -2,11 +2,13 @@
 ::
 SETLOCAL
 ::
-SET code=%~dp0..
+SET code=%~dp0
+SET code1=%code:~0,-8%
+::
 SET module=hpfs
 ::
 PING -n 5 127.0.0.1>nul
 PowerShell -WindowStyle Hidden -NoProfile -ExecutionPolicy Bypass -Command^
- "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -command ""&{Import-Module %code%; DispatchTasks %module%}""' -Verb RunAs}"
+ "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -command ""&{Import-Module %code1%; DispatchTasks %module%}""' -Verb RunAs}"
 ::
 ENDLOCAL&
