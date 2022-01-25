5a6a369c2b22b729013483a5b783a41a0bf3df07
andrewjorquera <andrewjorq@gmail.com>
2022-01-25 14:17:11 -0500

    add test warp octets

--- test/testpswarpoctets.ps1
+++ test/testpswarpoctets.ps1
@@ -1,33 +1,33 @@
 ﻿<# -------------------------------------------
- testpsmeanimage
+ testpswarpoctets
  created by: Andrew Jorquera
- Last Edit: 01.18.2022
+ Last Edit: 01.25.2022
  --------------------------------------------
  Description
- test if the methods of meanimage are 
+ test if the methods of warpoctets are 
  functioning as intended
  -------------------------------------------#>
 #
-Class testpsmeanimage {
+Class testpswarpoctets {
     #
     [string]$mpath 
     [string]$process_loc
     #
-    testpsmeanimage(){
+    testpswarpoctets(){
         #
         # Setup Testing
         #
         $this.importmodule()
         #
         $task = ('0', 'M21_1', $this.process_loc, $this.mpath)
-        $inp = meanimage $task
+        $inp = warpoctets $task
         #
         # Run Tests
         #
         $this.DownloadFilesTest($inp)
-        # $this.ShredDatTest($inp)
-        $this.ReturnDataTest($inp)
-        $this.CleanupTest($inp)
+        #$this.ShredDatTest($inp)
+        $this.GetWarpOctetsTest($inp)
+        $this.CleanupTest()
     }
     #
     importmodule(){
@@ -40,10 +40,8 @@ Class testpsmeanimage {
     [void]DownloadFilesTest($inp){
         Write-Host 'Starting Download Files Test'
         $inp.DownloadFiles()
-        $xmlpath = $inp.processvars[1] + '/' + $inp.sample.slideid + '/*.xml'
-        Write-Host 'xml path: ' $xmlpath
         $im3path = $inp.processvars[2] + '/../Scan1/MSI/*.im3'
-        if (!(@(Test-Path $xmlpath) -and @(Test-Path $im3path))) {
+        if (!(@(Test-Path $im3path))) {
             Throw 'Download Files Test Failed'
         }
         Write-Host 'Passed Download Files Test'
@@ -59,26 +57,14 @@ Class testpsmeanimage {
         Write-Host 'Passed Shred Dat Test'
     }
     #
-    [void]ReturnDataTest($inp){
-        Write-Host 'Starting Return Data Test'
-        Write-Host 'Processvars: '$inp.processvars
-        $sourcepath = $inp.processvars[0] + '\meanimage'
-        $returnpath = $inp.sample.im3folder() + '\meanimage'
-        Write-Host 'Source Path: ' $sourcepath
-        Write-Host 'Return Path: ' $returnpath
-        #
-        $inp.returndata()
-        if ($inp.processvars[4]) {
-            #
-            if (!(@(Test-Path $sourcepath))) {
-                Throw 'Return Data Test Failed - Source path does not exist'
-            }
-            #
-            if (!(@(Test-Path $returnpath))) {
-                Throw 'Return Data Test Failed - Return path does not exist'
-            }
+    [void]GetWarpOctetsTest($inp){
+        Write-Host 'Starting GetWarpOctets Test'
+        $output = $this.basepath + '\warping\octets\' + $this.slideid + '-all_overlap_octets.csv'
+        $inp.warpoctets()
+        if (!(test-path $output)){
+            Throw 'Warp Octets Test Failed - Output file does not exist'
         }
-        Write-Host 'Passed Return Data Test'
+        Write-Host 'Passed GetWarpOctets Test'
     }
     #
     [void]CleanupTest($inp){
@@ -92,9 +78,10 @@ Class testpsmeanimage {
         Write-Host 'Processing Folder Deleted'
         Write-Host 'Passed Cleanup Test'
     }
+    #
 }
 #
 # launch test and exit if no error found
 #
-$test = [testpsmeanimage]::new()
+$test = [testpswarpoctets]::new()
 exit 0
