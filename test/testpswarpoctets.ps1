f77176d4540c32cbd4a18e763502977a16d7f8b1
andrewjorquera <andrewjorq@gmail.com>
2022-01-31 15:27:38 -0500

    add parameter into cleanup tet

--- test/testpswarpoctets.ps1
+++ test/testpswarpoctets.ps1
@@ -25,7 +25,7 @@ Class testpswarpoctets {
         # Run Tests
         #
         $this.DownloadFilesTest($inp)
-        $this.CleanupTest()
+        $this.CleanupTest($inp)
     }
     #
     importmodule(){
