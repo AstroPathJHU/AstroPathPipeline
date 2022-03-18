3771dc39af23547f4cf928789d7d6e06a03cdbdb
beng1290 <beng1290@gmail.com>
2022-03-18 17:51:27 -0400

    working on workflow 4

--- test/testpsworkflow.ps1
+++ test/testpsworkflow.ps1
@@ -15,9 +15,8 @@ Class testpsworkflow : testtools {
     #
     testpsworkflow(): base(){
         #
-        #$password = ConvertTo-SecureString "MyPlainTextPassword" -AsPlainText -Force
-        #$cred = New-Object System.Management.Automation.PSCredential ("username", $password)  
-        $cred = [System.Net.CredentialCache]::DefaultNetworkCredentials
+        $password = ConvertTo-SecureString "MyPlainTextPassword" -AsPlainText -Force
+        $cred = New-Object System.Management.Automation.PSCredential ("username", $password)  
         #
         $this.testconstructors($cred)
         #  
