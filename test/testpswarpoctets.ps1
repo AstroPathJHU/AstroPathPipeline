661ba3db1c7a1f45064cac91e2822fafd5a3d3e6
andrewjorquera <andrewjorq@gmail.com>
2022-07-26 16:21:39 -0400

    test merge with main

--- test/testpswarpoctets.ps1
+++ test/testpswarpoctets.ps1
@@ -57,7 +57,7 @@ Class testpswarpoctets : testtools {
         $log = logger $this.mpath $this.module $this.slideid 
         #
         try {
-            warpoctets  $task | Out-Null
+            warpoctets $task | Out-Null
         } catch {
             Throw ('[warpoctets] construction with [1] input(s) failed. ' + $_.Exception.Message)
         }
