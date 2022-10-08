eadec4b2ce58f0b4efb28ad19e43fb046680fc3c
Ben <bgreen42@jhu.edu>
2022-10-08 17:59:15 -0400

    remove test clean up base in meanimage

--- test/testpsmeanimage.ps1
+++ test/testpsmeanimage.ps1
@@ -39,7 +39,7 @@ Class testpsmeanimage : testtools {
         $inp.sample.teststatus = $true  
         $this.checkcreatepyenv($inp.sample)
         $this.testprocessroot($inp, $true)
-        $this.testcleanupbase($inp)
+        #$this.testcleanupbase($inp)
         $this.comparepymeanimageinput($inp)
         $this.runpytaskpyerror($inp, $true)
         $this.testlogpyerror($inp)
