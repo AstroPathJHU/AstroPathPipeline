6f86834cf5e3f168b43d36c9e69e12018361ff5e
Maggie Eminizer <margaret.eminizer@gmail.com>
2022-10-13 12:39:36 -0400

    WIP commit: flatfield
    This should handle flatfield objects

--- astropath/hpfs/flatfield/flatfield.py
+++ astropath/hpfs/flatfield/flatfield.py
@@ -17,17 +17,6 @@ class FlatfieldBase(ImageStackBase) :
     The model is created by reading the meanimages and other data for each slide
     """
 
-class FlatfieldComponentTiff(FlatfieldBase,ImageStackComponentTiff) :
-    """
-    Class representing a flatfield model built from component tiff mean images
-    """
-
-class FlatfieldIm3(FlatfieldBase,ImageStackIm3) :
-    """
-    Class representing the flatfield model for a group of slides
-    The model is created by reading the meanimages and other data for each slide
-    """
-
     #################### PUBLIC FUNCTIONS ####################
 
     def __init__(self,*args,**kwargs) :
@@ -127,22 +116,27 @@ class FlatfieldIm3(FlatfieldBase,ImageStackIm3) :
         if len(self.__field_logs)>0 :
             with cd(workingdirpath) :
                 writetable(f'{CONST.FIELDS_USED_CSV_FILENAME}',self.__field_logs)
-        #make some plots of the image layers and the pixel intensities
-        plotdir_path = workingdirpath / f'{UNIV_CONST.FLATFIELD_DIRNAME}_{version}_plots'
-        plotdir_path.mkdir(exist_ok=True)
-        plot_image_layers(self.__flatfield_image,f'{UNIV_CONST.FLATFIELD_DIRNAME}_{version}',plotdir_path)
-        plot_image_layers(self.__flatfield_image_err,
-                          f'{UNIV_CONST.FLATFIELD_DIRNAME}_{version}_uncertainty',plotdir_path)
-        if self.mask_stack is not None :
-            plot_image_layers(self.mask_stack,f'{UNIV_CONST.FLATFIELD_DIRNAME}_{version}_mask_stack',plotdir_path)
-        flatfield_image_pixel_intensity_plot(samp,self.__flatfield_image,version,plotdir_path)
-        #make the summary PDF
-        latex_summary = FlatfieldLatexSummary(self.__flatfield_image,plotdir_path,version)
-        latex_summary.build_tex_file()
-        check = latex_summary.compile()
-        if check!=0 :
-            warnmsg = 'WARNING: failed while compiling flatfield summary LaTeX file into a PDF. '
-            warnmsg+= f'tex file will be in {latex_summary.failed_compilation_tex_file_path}'
+        try :
+            #make some plots of the image layers and the pixel intensities
+            plotdir_path = workingdirpath / f'{UNIV_CONST.FLATFIELD_DIRNAME}_{version}_plots'
+            plotdir_path.mkdir(exist_ok=True)
+            plot_image_layers(self.__flatfield_image,f'{UNIV_CONST.FLATFIELD_DIRNAME}_{version}',plotdir_path)
+            plot_image_layers(self.__flatfield_image_err,
+                              f'{UNIV_CONST.FLATFIELD_DIRNAME}_{version}_uncertainty',plotdir_path)
+            if self.mask_stack is not None :
+                plot_image_layers(self.mask_stack,f'{UNIV_CONST.FLATFIELD_DIRNAME}_{version}_mask_stack',plotdir_path)
+            flatfield_image_pixel_intensity_plot(samp,self.__flatfield_image,version,plotdir_path)
+            #make the summary PDF
+            latex_summary = FlatfieldLatexSummary(self.__flatfield_image,plotdir_path,version)
+            latex_summary.build_tex_file()
+            check = latex_summary.compile()
+            if check!=0 :
+                warnmsg = 'WARNING: failed while compiling flatfield summary LaTeX file into a PDF. '
+                warnmsg+= f'tex file will be in {latex_summary.failed_compilation_tex_file_path}'
+                self.logger.warning(warnmsg)
+        except Exception as e :
+            warnmsg = 'WARNING: failed to write out some optional plots (scripts will need to be run separately). '
+            warnmsg+= f'Exception: {e}'
             self.logger.warning(warnmsg)
 
     #################### PROPERTIES ####################
@@ -153,3 +147,16 @@ class FlatfieldIm3(FlatfieldBase,ImageStackIm3) :
     @property
     def flatfield_image_err(self) :
         return self.__flatfield_image_err
+
+class FlatfieldComponentTiff(FlatfieldBase,ImageStackComponentTiff) :
+    """
+    Class representing a flatfield model built from component tiff mean images
+    """
+    pass
+
+class FlatfieldIm3(FlatfieldBase,ImageStackIm3) :
+    """
+    Class representing the flatfield model for a group of slides
+    The model is created by reading the meanimages and other data for each slide
+    """
+    pass
