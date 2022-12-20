0efc5c4441779e302cb103ad459909cdf48570cb
Heshy Roskes <heshyr@gmail.com>
2022-12-20 13:49:21 -0500

    find circles

--- astropath/slides/annowarp/tenxannowarp.py
+++ astropath/slides/annowarp/tenxannowarp.py
@@ -1,4 +1,4 @@
-import matplotlib.patches, matplotlib.pyplot as plt
+import cv2, matplotlib.patches, matplotlib.pyplot as plt, numpy as np, skimage.filters
 from ...shared.tenx import TenXSampleBase
 
 class TenXAnnoWarp(TenXSampleBase):
@@ -22,8 +22,44 @@ class TenXAnnoWarp(TenXSampleBase):
     yc = float(spot.imageY / self.onepixel)
     dia = float(spot.dia / self.onepixel)
 
-    fig, ax = plt.subplots()
     im, (x1, y1, x2, y2) = self.circle_subplot(spot)
-    plt.imshow(im, extent=(x1, x2, y2, y1))
     circle = matplotlib.patches.Circle((xc, yc), dia/2, alpha=0.3, color='r')
     ax.add_patch(circle)
+
+  def findcircle(self, spot, draw=False):
+    wsi, (x1, y1, x2, y2) = self.circle_subplot(spot)
+    gray = cv2.cvtColor(wsi, cv2.COLOR_BGR2GRAY)
+    bw = np.where(gray>120, 255, 0).astype(np.uint8)
+    smooth = (skimage.filters.gaussian(bw, sigma=10, mode='nearest') * 255).astype(np.uint8)
+    xc = float(spot.imageX / self.onepixel)
+    yc = float(spot.imageY / self.onepixel)
+    dia = float(spot.dia / spot.onepixel)
+    circles = cv2.HoughCircles(
+      gray,cv2.HOUGH_GRADIENT,1,20,
+      param1=50,param2=50,minRadius=int(dia*.9//2),maxRadius=int(dia*1.1//2)
+    )
+    if circles is not None:
+      circle = circles[0][0]
+      circle[0] += x1
+      circle[1] += y1
+    else:
+      circle = None
+
+    if draw:
+      fig, ax = plt.subplots()
+      plt.imshow(
+        wsi,
+        #smooth,
+        extent=(x1, x2, y2, y1),
+      )
+      patchkwargs = {
+        #"alpha": 0.3,
+        "fill": False,
+        "linewidth": 2,
+      }
+      if circle is not None:
+        ax.add_patch(matplotlib.patches.Circle((circle[0], circle[1]), circle[2], color='b', **patchkwargs))
+      ax.add_patch(matplotlib.patches.Circle((xc, yc), dia/2, color='r', **patchkwargs))
+      plt.show()
+
+    return circle
