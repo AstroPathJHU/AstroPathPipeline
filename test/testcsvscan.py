3a1c3293b46873753f6790fcf80df1320fbda55c
Heshy Roskes <heshyr@gmail.com>
2023-01-29 08:35:50 -0500

    csvscan skip cells and annotations

--- test/testcsvscan.py
+++ test/testcsvscan.py
@@ -12,49 +12,54 @@ thisfolder = pathlib.Path(__file__).parent
 class TestCsvScan(TestBaseCopyInput, TestBaseSaveOutput):
   @classmethod
   def filestocopy(cls):
-    testroot = thisfolder/"test_for_jenkins"/"csvscan"/"Clinical_Specimen_0"
+    maintestroot = thisfolder/"test_for_jenkins"/"csvscan"
     dataroot = thisfolder/"data"
+    for folder in "everything", "skip_cells", "skip_annotations", "skip_cells_annotations":
+      testroot = maintestroot/folder/"Clinical_Specimen_0"
 
-    for foldername in "Batch", "Clinical", "Ctrl", pathlib.Path("Control_TMA_1372_111_06.19.2019")/"dbload":
-      old = dataroot/foldername
-      new = testroot/foldername
-      for csv in old.glob("*.csv"):
-        yield csv, new
+      for foldername in "Batch", "Clinical", "Ctrl", pathlib.Path("Control_TMA_1372_111_06.19.2019")/"dbload":
+        old = dataroot/foldername
+        new = testroot/foldername
+        for csv in old.glob("*.csv"):
+          yield csv, new
 
-    for SlideID in "M206",:
-      Scan = {
-        "M206": 1,
-      }[SlideID]
-      newdbload = testroot/SlideID/"dbload"
-      newtables = testroot/SlideID/"inform_data"/"Phenotyped"/"Results"/"Tables"
+      for SlideID in "M206",:
+        Scan = {
+          "M206": 1,
+        }[SlideID]
+        newdbload = testroot/SlideID/"dbload"
+        newtables = testroot/SlideID/"inform_data"/"Phenotyped"/"Results"/"Tables"
 
-      for olddbload in dataroot/SlideID/"dbload", thisfolder/"data"/"reference"/"geom"/SlideID/"dbload", thisfolder/"data"/"reference"/"annowarp"/SlideID/"dbload":
-        for csv in olddbload.glob("*.csv"):
-          if csv == dataroot/SlideID/"dbload"/f"{SlideID}_vertices.csv": continue
-          if csv == dataroot/SlideID/"dbload"/f"{SlideID}_regions.csv": continue
-          yield csv, newdbload
+        for olddbload in dataroot/SlideID/"dbload", thisfolder/"data"/"reference"/"annowarp"/SlideID/"dbload":
+          for csv in olddbload.glob("*.csv"):
+            if csv == dataroot/SlideID/"dbload"/f"{SlideID}_vertices.csv": continue
+            if csv == dataroot/SlideID/"dbload"/f"{SlideID}_regions.csv": continue
+            if "annotations" in folder and csv.name in (f"{SlideID}_annotationinfo.csv", f"{SlideID}_annotations.csv", f"{SlideID}_annowarp.csv", f"{SlideID}_annowarp-stitch.csv", f"{SlideID}_regions.csv", f"{SlideID}_vertices.csv"): continue
+            yield csv, newdbload
 
-      oldtables = dataroot/SlideID/"inform_data"/"Phenotyped"/"Results"/"Tables"
-      for csv in oldtables.glob("*.csv"):
-        yield csv, newtables
+        oldtables = dataroot/SlideID/"inform_data"/"Phenotyped"/"Results"/"Tables"
+        if "cells" not in folder:
+          for csv in oldtables.glob("*.csv"):
+            yield csv, newtables
 
-      yield dataroot/SlideID/"im3"/f"{SlideID}-mean.csv", testroot/SlideID/"im3"
-      yield dataroot/SlideID/"im3"/f"Scan{Scan}"/f"{SlideID}_Scan{Scan}_annotations.xml", testroot/SlideID/"im3"/f"Scan{Scan}"
-      yield dataroot/SlideID/"im3"/"xml"/f"{SlideID}.Parameters.xml", testroot/SlideID/"im3"/"xml"
+        yield dataroot/SlideID/"im3"/f"{SlideID}-mean.csv", testroot/SlideID/"im3"
+        yield dataroot/SlideID/"im3"/f"Scan{Scan}"/f"{SlideID}_Scan{Scan}_annotations.xml", testroot/SlideID/"im3"/f"Scan{Scan}"
+        yield dataroot/SlideID/"im3"/"xml"/f"{SlideID}.Parameters.xml", testroot/SlideID/"im3"/"xml"
 
   @property
   def outputfilenames(self):
-    return [
-      thisfolder/"test_for_jenkins"/"csvscan"/"Clinical_Specimen_0"/SlideID/"dbload"/f"{SlideID}_{csv}.csv"
-      for csv in ("loadfiles",)
-      for SlideID in ("M206",)
-    ] + [
-      thisfolder/"test_for_jenkins"/"csvscan"/"Clinical_Specimen_0"/SlideID/"logfiles"/f"{SlideID}-csvscan.log"
-      for SlideID in ("M206",)
-    ] + [
-      thisfolder/"test_for_jenkins"/"csvscan"/"Clinical_Specimen_0"/"dbload"/"project0_loadfiles.csv",
-      thisfolder/"test_for_jenkins"/"csvscan"/"Clinical_Specimen_0"/"logfiles"/"csvscan.log",
-    ]
+    for folder in "everything", "skip_cells", "skip_annotations", "skip_cells_annotations":
+      yield from [
+        thisfolder/"test_for_jenkins"/"csvscan"/folder/"Clinical_Specimen_0"/SlideID/"dbload"/f"{SlideID}_{csv}.csv"
+        for csv in ("loadfiles",)
+        for SlideID in ("M206",)
+      ] + [
+        thisfolder/"test_for_jenkins"/"csvscan"/folder/"Clinical_Specimen_0"/SlideID/"logfiles"/f"{SlideID}-csvscan.log"
+        for SlideID in ("M206",)
+      ] + [
+        thisfolder/"test_for_jenkins"/"csvscan"/folder/"Clinical_Specimen_0"/"dbload"/"project0_loadfiles.csv",
+        thisfolder/"test_for_jenkins"/"csvscan"/folder/"Clinical_Specimen_0"/"logfiles"/"csvscan.log",
+      ]
 
   def setUp(self):
     stack = self.__stack = contextlib.ExitStack()
@@ -64,38 +69,43 @@ class TestCsvScan(TestBaseCopyInput, TestBaseSaveOutput):
   
       from astropath.utilities.version import astropathversion
   
-      testroot = thisfolder/"test_for_jenkins"/"csvscan"/"Clinical_Specimen_0"
-      dataroot = thisfolder/"data"
-      for SlideID in slideids:
-        logfolder = testroot/SlideID/"logfiles"
-        logfolder.mkdir(exist_ok=True, parents=True)
-        for module in "annowarp", "geom", "geomcell", "csvscan", "copyannotationinfo":
-          now = datetime.datetime.now()
-          if module == "csvscan":
-            starttime = now - datetime.timedelta(seconds=15)
-          else:
-            starttime = now
-          endtime = starttime + datetime.timedelta(seconds=30)
+      maintestroot = thisfolder/"test_for_jenkins"/"csvscan"
+      for folder in "everything", "skip_cells", "skip_annotations", "skip_cells_annotations":
+        dataroot = thisfolder/"data"
+        testroot = maintestroot/folder/"Clinical_Specimen_0"
+        for SlideID in slideids:
+          logfolder = testroot/SlideID/"logfiles"
+          logfolder.mkdir(exist_ok=True, parents=True)
+          for module in "align", "annowarp", "geomcell", "csvscan", "copyannotationinfo":
+            if "anno" in module and "annotations" in folder: continue
+            if module == "geomcell" and "cells" in folder: continue
 
-          filename = logfolder/f"{SlideID}-{module}.log"
-          assert stack.enter_context(job_lock.JobLock(filename))
-          with open(filename, "w", newline="") as f:
-            f.write(f"0;0;{SlideID};{module} {astropathversion};{starttime.strftime(MyLogger.dateformat)}\r\n")
-            f.write(f"0;0;{SlideID};end {module};{endtime.strftime(MyLogger.dateformat)}\r\n")
+            now = datetime.datetime.now()
+            if module == "csvscan":
+              starttime = now - datetime.timedelta(seconds=15)
+            else:
+              starttime = now
+            endtime = starttime + datetime.timedelta(seconds=30)
 
-        dbloadfolder = testroot/SlideID/"dbload"
-        dbloadfolder.mkdir(exist_ok=True, parents=True)
-        (dbloadfolder/f"{SlideID}_loadfiles.csv").touch()
+            filename = logfolder/f"{SlideID}-{module}.log"
+            assert stack.enter_context(job_lock.JobLock(filename))
+            with open(filename, "w", newline="") as f:
+              f.write(f"0;0;{SlideID};{module} {astropathversion};{starttime.strftime(MyLogger.dateformat)}\r\n")
+              f.write(f"0;0;{SlideID};end {module};{endtime.strftime(MyLogger.dateformat)}\r\n")
 
-        assert CsvScanSample.getrunstatus(SlideID=SlideID, root=dataroot, logroot=testroot, dbloadroot=testroot)
-  
-      sampledef = testroot/"sampledef.csv"
-      assert stack.enter_context(job_lock.JobLock(sampledef))
-      with open(dataroot/"sampledef.csv") as f, open(sampledef, "w") as newf:
-        for line in f:
-          if line.strip() and line.split(",")[1] in ("SlideID",) + slideids:
-            newf.write(line)
-  
+          dbloadfolder = testroot/SlideID/"dbload"
+          dbloadfolder.mkdir(exist_ok=True, parents=True)
+          (dbloadfolder/f"{SlideID}_loadfiles.csv").touch()
+
+          assert CsvScanSample.getrunstatus(SlideID=SlideID, root=dataroot, logroot=testroot, dbloadroot=testroot)
+
+        sampledef = testroot/"sampledef.csv"
+        assert stack.enter_context(job_lock.JobLock(sampledef))
+        with open(dataroot/"sampledef.csv") as f, open(sampledef, "w") as newf:
+          for line in f:
+            if line.strip() and line.split(",")[1] in ("SlideID",) + slideids:
+              newf.write(line)
+    
     except:
       stack.close()
       raise
@@ -104,10 +114,18 @@ class TestCsvScan(TestBaseCopyInput, TestBaseSaveOutput):
     super().tearDown()
     self.__stack.close()
 
-  def testCsvScan(self, SlideID="M206", units="safe", selectrectangles=[1], skipcheck=False, nolog=False):
-    root = thisfolder/"test_for_jenkins"/"csvscan"/"Clinical_Specimen_0"
+  def testCsvScan(self, SlideID="M206", units="safe", selectrectangles=[1], skipcheck=False, nolog=False, skipcells=False, skipannotations=False):
+    if skipcells and skipannotations:
+      subdir = "skip_cells_annotations"
+    elif skipcells:
+      subdir = "skip_cells"
+    elif skipannotations:
+      subdir = "skip_annotations"
+    else:
+      subdir = "everything"
+    root = thisfolder/"test_for_jenkins"/"csvscan"/subdir/"Clinical_Specimen_0"
     geomroot = thisfolder/"data"/"reference"/"geomcell"
-    args = [os.fspath(root), "--geomroot", os.fspath(geomroot), "--units", units, "--sampleregex", SlideID, "--debug", "--allow-local-edits", "--do-global-csv"]
+    args = [os.fspath(root), "--units", units, "--sampleregex", SlideID, "--debug", "--allow-local-edits", "--do-global-csv"]
     if selectrectangles is not None:
       args.append("--selectrectangles")
       for rid in selectrectangles: args.append(str(rid))
@@ -115,11 +133,17 @@ class TestCsvScan(TestBaseCopyInput, TestBaseSaveOutput):
       args.append("--skip-check")
     if nolog:
       args.append("--no-log")
+    if skipcells:
+      args.append("--skip-cells")
+    else:
+      args += ["--geomroot", os.fspath(geomroot)]
+    if skipannotations:
+      args.append("--skip-annotations")
     CsvScanCohort.runfromargumentparser(args=args)
 
     s = CsvScanSample(root=root, geomroot=geomroot, samp=SlideID)
     filename = s.csv("loadfiles")
-    reffolder = thisfolder/"data"/"reference"/"csvscan"/SlideID
+    reffolder = thisfolder/"data"/"reference"/"csvscan"/subdir/SlideID
     reference = reffolder/filename.name
 
     try:
@@ -131,13 +155,14 @@ class TestCsvScan(TestBaseCopyInput, TestBaseSaveOutput):
           folder = s.geomroot/s.SlideID
         row.filename = row.filename.relative_to(folder).as_posix()
       targetcommonroot = commonroot(*(row.filename for row in targetrows))
+      if targetcommonroot.name == "dbload": targetcommonroot = targetcommonroot.parent
       for row in targetrows:
         row.filename = row.filename.relative_to(targetcommonroot).as_posix()
       for row, target in more_itertools.zip_equal(rows, targetrows):
         assertAlmostEqual(row, target)
 
       filename = s.root/"dbload"/"project0_loadfiles.csv"
-      reference = thisfolder/"data"/"reference"/"csvscan"/filename.name
+      reference = thisfolder/"data"/"reference"/"csvscan"/subdir/filename.name
       rows = s.readtable(filename, LoadFile, header=False, checkorder=True, checknewlines=True)
       targetrows = s.readtable(reference, LoadFile, header=False, checkorder=True, checknewlines=True)
       for row in rows:
@@ -162,8 +187,11 @@ class TestCsvScan(TestBaseCopyInput, TestBaseSaveOutput):
     else:
       self.removeoutput()
 
-  def testCsvScanFastUnits(self, **kwargs):
-    self.testCsvScan(units="fast", nolog=True, **kwargs)
+  def testCsvScanSkipAnnotations(self, **kwargs):
+    self.testCsvScan(units="fast", skipcheck=True, skipannotations=True, nolog=True, **kwargs)
 
-  def testCsvScanNoCheck(self, **kwargs):
-    self.testCsvScan(skipcheck=True)
+  def testCsvScanSkipCells(self, **kwargs):
+    self.testCsvScan(skipcheck=True, skipcells=True)
+
+  def testCsvScanSkipCellsAnnotations(self, **kwargs):
+    self.testCsvScan(skipcells=True, skipannotations=True, **kwargs)
