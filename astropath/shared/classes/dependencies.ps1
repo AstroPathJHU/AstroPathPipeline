fca7ba0de5d920f6bfae33782611749cdb1c2a03
andrewjorquera <andrewjorq@gmail.com>
2023-03-14 15:47:32 -0400

    remove debug lines

--- astropath/shared/classes/dependencies.ps1
+++ astropath/shared/classes/dependencies.ps1
@@ -51,9 +51,6 @@
     #
     [void]getlogstatussub($cmodule){
         #
-        if ($cmodule -match 'shredxml') {
-            Write-host '****dependency check****'
-        }
         $logoutput = $this.checkloginit($cmodule, $false)
         #
         if ($logoutput[1]){
@@ -61,9 +58,6 @@
         } elseif ($logoutput) {
             #
             $check = $this.checkmodule($cmodule)
-            if ($cmodule -match 'shredxml') {
-                Write-host $check
-            }
             #
             switch ($check){
                 1 {
@@ -164,16 +158,11 @@
             $cmoduleinfo =  $this.moduleinfo.($cmodule).($antibody)
         } else {
             $cmoduleinfo =  $this.moduleinfo.($cmodule)
-            Write-host 'module info:'
-            Write-host ($cmoduleinfo | Out-String)
         }
         #
         $startdate = $cmoduleinfo.StartTime
         $finishdate = $cmoduleinfo.FinishTime
         $errorline = $cmoduleinfo.Errorline
-        Write-host 'startdate: ' $startdate
-        Write-host 'finishdate: ' $finishdate
-        Write-host 'errorline: ' $errorline
         #
         return ($this.deflogstatus($startdate,
             $finishdate, $errorline, $dependency))
@@ -450,15 +439,11 @@
         #
         if ($cmodule -notmatch $this.nolog_modules){
             if ($this.checklog($cmodule, $true)){
-                write-host 'check log failed'
                 return 2
             }  
         }
         #
         if (!$this.('test' + $cmodule + 'files')()){
-            if ($cmodule -match 'shredxml') {
-                write-host 'test files failed'
-            }
             return 2
         }
         #
