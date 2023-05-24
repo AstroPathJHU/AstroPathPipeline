842d7059e40e78b3a12371965363d7673cfdc52e
Heshy Roskes <heshyr@gmail.com>
2023-05-24 14:00:44 -0400

    testing

--- astropath/slides/deepzoom/deepzoomsampletmapercore.py
+++ astropath/slides/deepzoom/deepzoomsampletmapercore.py
@@ -60,7 +60,7 @@ class DeepZoomSampleBaseTMAPerCore(DbloadSampleBase, ZoomFolderSampleBase, DeepZ
     img = array_to_vips_image(array)
     wsi = img.affine([1, 0, 0, 1], idx=TMAcore.x1, idy=TMAcore.y1)
     from ...utilities.miscimage import vips_image_to_array
-    print(img.shape)
+    print(array.shape)
     print(vips_image_to_array(wsi).shape)
     assert 0
     wsi.dzsave(os.fspath(dest), suffix=".png", background=0, depth="onetile", overlap=0, tile_size=self.tilesize)
